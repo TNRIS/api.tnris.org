ca0a8f195c6615409f4c95648e4eb70244cc8137
John Haney <geojwh@gmail.com>
2018-08-16 14:42:46 -0500

    port over data concierge historical aerials app to lore
    working on exporting and loading fixtures to data.tnris.org RDS

---
+++ src/data_hub/lore/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'lore.apps.ApiConfig'
---
+++ src/data_hub/lore/urls.py
@@ -0,0 +1,35 @@
+"""historical_aerials URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.schemas import get_schema_view
+
+from .viewsets import CountyViewSet, ProductViewSet, MapserverViewSet
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'counties/?', CountyViewSet)
+router.register(r'records/?', ProductViewSet, base_name="Product")
+router.register(r'mapserver/', MapserverViewSet, base_name="Mapserver")
+
+schema_view = get_swagger_view(title='Historical Imagery API')
+# schema_view = get_schema_view(title='Historical Imagery API')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('schema/', schema_view),
+    path('historical_aerials-auth/?', include('rest_framework.urls', namespace='rest_framework'))
+]

138f2bdc8598e86a70af915b204a92134dce5e86
Brez <smudgeservices@gmail.com>
2018-08-23 09:10:36 -0500

    debug off. prod env variables

--- docker-entrypoint.d/00-start-app.sh
+++ docker-entrypoint.d/00-start-app.sh
@@ -5,11 +5,10 @@ export DB_NAME=`/envs/data-tnris-org/bin/aws ssm get-parameters --name /apps/dat
 export DB_USER=`/envs/data-tnris-org/bin/aws ssm get-parameters --name /apps/data_tnris_org/db_user --with-decryption --region us-east-1 --output text | awk '{print $6}'`
 export DB_PASSWORD=`/envs/data-tnris-org/bin/aws ssm get-parameters --name /apps/data_tnris_org/db_password --with-decryption --region us-east-1 --output text | awk '{print $6}'`
 export DB_HOST=`/envs/data-tnris-org/bin/aws ssm get-parameters --name /apps/data_tnris_org/db_host --with-decryption --region us-east-1 --output text | awk '{print $6}'`
+export DB_PORT=`/envs/data-tnris-org/bin/aws ssm get-parameters --name /apps/data_tnris_org/db_port --with-decryption --region us-east-1 --output text | awk '{print $6}'`
 
 export AWS_ACCESS_KEY_ID=`/envs/data-tnris-org/bin/aws ssm get-parameters --name /apps/data_tnris_org/aws_access_key_id --with-decryption --region us-east-1 --output text | awk '{print $6}'`
 export AWS_SECRET_ACCESS_KEY=`/envs/data-tnris-org/bin/aws ssm get-parameters --name /apps/data_tnris_org/aws_secret_access_key --with-decryption --region us-east-1 --output text | awk '{print $6}'`
 
-# export AWS_STORAGE_BUCKET_NAME=`/envs/data-tnris-org/bin/aws ssm get-parameters --name /apps/data_tnris_org/aws_storage_bucket_name --region us-east-1 --output text | awk '{print $6}'`
-#
 # eval /src/data_concierge/manage.py collectstatic --noinput "$@"
 eval /usr/bin/supervisord "$@"

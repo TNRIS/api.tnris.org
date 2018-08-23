19329aaf39d673a48654d27b74d2e8abe7f022e0
Brez <smudgeservices@gmail.com>
2018-08-23 08:08:23 -0500

    secrets example

--- src/data_hub/set-env-secrets-example.sh
+++ src/data_hub/set-env-secrets-example.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 export DB_PASSWORD="<database password here>"
-export AWS_ACCESS_KEY_ID='<data.tnris.org user aws access key>'
-export AWS_SECRET_ACCESS_KEY='<data.tnris.org user aws secret key>'
+export DB_NAME="<database name>"
+export DB_USER="<database user>"
+export DB_HOST='<database host address>'
+export DB_PORT='<database port>'
 
 # the following is only used within ETL scripts (specifically, migrate.py)
 export TNRIS_REPO='<local path to cloned tnris.org website repo>'
+
+export AWS_ACCESS_KEY_ID='<data.tnris.org user aws access key>'
+export AWS_SECRET_ACCESS_KEY='<data.tnris.org user aws secret key>'

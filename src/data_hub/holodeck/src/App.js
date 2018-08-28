9c6afddd15f8048a6c420900f105eac10b08bdee
Brez <smudgeservices@gmail.com>
2018-08-28 10:43:57 -0500

    reducer index -> rootReducer

--- src/data_hub/holodeck/src/App.js
+++ src/data_hub/holodeck/src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-import rootReducer from './reducers';
+import rootReducer from './reducers/rootReducer';
 
 import Catalog from './components/Catalog';
 import Map from './components/Map';

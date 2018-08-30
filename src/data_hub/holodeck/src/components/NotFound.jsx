3ffee99343760d0bdbe9e1f36b654f6fedaba08c
Brez <smudgeservices@gmail.com>
2018-08-30 12:06:39 -0500

    notfound was refound so react handles it rather than django.

---
+++ src/data_hub/holodeck/src/components/NotFound.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+class NotFound extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="notfound-component">
+        <Header />
+          <div className="notfound">
+            <img
+              src="http://www.startrek.com/uploads/assets/db_articles/d3c7e2f13d99edef700ed6b48fbfb694579299d2.jpg"
+              alt="Lieutenant Commander Data"
+              className="notfound-image"
+              />
+            <h2>ERROR 404: Not Found</h2>
+            <p>The page you're looking for does not exist.</p>
+          </div>
+        <Footer className='footer navbarFixedBottom' />
+      </div>
+    )
+  }
+}
+
+export default NotFound

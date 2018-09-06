b8f89617c3d4dbd1603e4360cc396c1cbd65f0e8
Brez <smudgeservices@gmail.com>
2018-09-06 14:19:49 -0500

    grid span

--- src/data_hub/holodeck/src/components/CollectionCard.jsx
+++ src/data_hub/holodeck/src/components/CollectionCard.jsx
@@ -5,14 +5,14 @@ export default class DialogContent extends React.Component {
     return (
       <div className='collection-card-component mdc-layout-grid'>
         <div className='mdc-layout-grid__inner'>
-          <h4 className='mdc-typography--headline4 mdc-layout-grid__cell mdc-layout-grid__cell--span12'>
+          <h4 className='mdc-typography--headline4 mdc-layout-grid__cell mdc-layout-grid__cell--span-12'>
             {this.props.collection.name}
           </h4>
         </div>
           <img src={this.props.collection.overview_image} alt=''
-               className='collection-card__image mdc-layout-grid__cell'/>
+               className='collection-card__image mdc-layout-grid__cell--span-12'/>
         <div className='mdc-layout-grid__inner'>
-          <p className='mdc-typography__body2 mdc-layout-grid__cell'>
+          <p className='mdc-typography__body2 mdc-layout-grid__cell--span-12'>
             {this.props.collection.description}
           </p>
         </div>

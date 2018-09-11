4f85a67c7e0baa57f2fe4ce0e6edb7cda7669b26
Brez <smudgeservices@gmail.com>
2018-09-11 13:58:11 -0500

    sort menu fires actions

--- src/data_hub/holodeck/src/actions/sortActions.js
+++ src/data_hub/holodeck/src/actions/sortActions.js
@@ -4,10 +4,18 @@ import {
   SET_SORT_ZA,
 } from '../constants/sortActionTypes';
 
-export const setSortAZ = () => ({
-  type: SET_SORT_AZ
-});
+export const setSortAZ = () => {
+  return (dispatch) => {
+    dispatch({
+      type: SET_SORT_AZ
+    })
+  }
+};
 
-export const setSortZA = () => ({
-  type: SET_SORT_ZA
-});
+export const setSortZA = () => {
+  return (dispatch) => {
+    dispatch({
+      type: SET_SORT_ZA
+    })
+  }
+};

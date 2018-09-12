82a651b17f5b023f09cde427a7634309463a236f
Brez <smudgeservices@gmail.com>
2018-09-12 15:59:25 -0500

    initial template use on cards

--- src/data_hub/holodeck/src/components/CatalogCard.jsx
+++ src/data_hub/holodeck/src/components/CatalogCard.jsx
@@ -15,8 +15,10 @@ export default class CatalogCard extends React.Component {
   }
 
   render() {
+    const cardClass = 'catalog-card-component mdc-card mdc-card__primary-action ' + this.props.collection.template;
+
     return (
-      <div className='catalog-card-component mdc-card mdc-card__primary-action'
+      <div className={cardClass}
            onClick={this.cardClicked}>
         <div
           className='catalog-card__media mdc-card__media mdc-card__media--16-9'
@@ -24,18 +26,7 @@ export default class CatalogCard extends React.Component {
         </div>
         <p className='catalog-card__headline mdc-typography--body1'>{this.props.collection.name}</p>
         <p className='catalog-card__body mdc-typography__body2'>{this.props.collection.short_description}</p>
-        <div className='catalog-card__actions mdc-card__actions'>
-          <div className='mdc-card__action-buttons'>
-            <button className='mdc-button mdc-card__action mdc-card__action--button'
-                    onClick={e => e.stopPropagation()}>
-                    Action 1
-            </button>
-            <button className='mdc-button mdc-card__action mdc-card__action--button'
-                    onClick={e => e.stopPropagation()}>
-                    Action 2
-            </button>
-          </div>
-        </div>
+        <div className='catalog-card__footer'></div>
       </div>
     );
   }

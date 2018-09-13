ede03bc5a6d6523f4413b853b935285008240ecb
Brez <smudgeservices@gmail.com>
2018-09-13 15:54:50 -0500

    toppappbar install and initial implementation

--- src/data_hub/holodeck/src/components/Header.jsx
+++ src/data_hub/holodeck/src/components/Header.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
+import {MDCTopAppBar} from '@material/top-app-bar/index';
 import SortContainer from '../containers/SortContainer';
 
-import logo from '../images/tnris_logo_white.png'
+import tnrisLogo from '../images/tnris.png'
+import twdbLogo from '../images/twdb_splash.png'
 
 export default class Header extends React.Component {
 
+  componentDidMount() {
+    this.topAppBarElement = document.querySelector('.mdc-top-app-bar');
+    this.topAppBar = new MDCTopAppBar(this.topAppBarElement);
+  }
+
   render() {
     return (
-      <div className='header-component'>
-        <div className='header-title'>
-          <span className='header-title__tnris'>Texas Natural Resources Information System</span>
-          <a className='header-title__twdb' href="http://www.twdb.texas.gov/">A Division of the Texas Water Development Board</a>
+      <header className="header-component mdc-top-app-bar">
+        <div className="header-title mdc-top-app-bar__row">
+          <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            <span className='header-title__tnris'>Texas Natural Resources Information System</span>
+          </section>
+          <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+            <a className='header-title__twdb' href="http://www.twdb.texas.gov/">A Division of the Texas Water Development Board</a>
+          </section>
         </div>
-        <div className='header-nav'>
-          <div className='header-nav__logos'>
-            <a href="https://tnris.org">
-              <img src={logo} alt="TNRIS.org Logo" className="tnris-logo" />
+        <div className="header-nav mdc-top-app-bar__row">
+          <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            <i className="material-icons mdc-top-app-bar__navigation-icon">menu</i>
+            <a href="https://tnris.org" className="mdc-top-app-bar__action-item">
+              <img src={tnrisLogo} aria-label="TNRIS Logo" alt="TNRIS Logo" className="logo" />
             </a>
-          </div>
-          <div className='header-nav__buttons'>
-            <SortContainer />
-          </div>
+            <span className="mdc-top-app-bar__title">Data Holodeck</span>
+            <div className='header-nav__buttons'>
+              <SortContainer />
+            </div>
+          </section>
+          <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+
+            <a href="http://www.twdb.texas.gov/" className="mdc-top-app-bar__action-item">
+              <img src={twdbLogo} aria-label="TWDB Logo" alt="TWDB Logo" className="logo" />
+            </a>
+          </section>
         </div>
-      </div>
+      </header>
     );
   }
 }

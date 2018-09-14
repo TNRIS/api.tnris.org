eff3bc79b1de411b9de3057e3494c8a31ae0e4af
Brez <smudgeservices@gmail.com>
2018-09-14 09:56:27 -0500

    dialog css

--- src/data_hub/holodeck/src/components/TnrisOrderTemplate.jsx
+++ src/data_hub/holodeck/src/components/TnrisOrderTemplate.jsx
@@ -10,16 +10,18 @@ export default class TnrisOrderTemplate extends React.Component {
     return (
       <div className='tnris-order-template mdc-layout-grid'>
         <div className='mdc-layout-grid__inner'>
-          <h4 className='mdc-typography--headline4 mdc-layout-grid__cell mdc-layout-grid__cell--span-12'>
+          <h4 className='mdc-typography--headline4 mdc-layout-grid__cell'>
             {this.props.collection.name}
           </h4>
         </div>
+        <div className='mdc-layout-grid__inner'>
           <img
             src={this.props.collection.overview_image}
             alt='Dataset Overview'
-            className='collection-dialog__image mdc-layout-grid__cell--span-12'/>
+            className='collection-dialog__image mdc-layout-grid__cell'/>
+        </div>
         <div className='mdc-layout-grid__inner'>
-          <p className='mdc-typography__body2 mdc-layout-grid__cell--span-12'>
+          <p className='mdc-typography__body2 mdc-layout-grid__cell'>
             {this.props.collection.description}
           </p>
         </div>

e84227656523f088fee151d85fc8d2d4956de241
Brez <smudgeservices@gmail.com>
2018-09-14 11:35:38 -0500

    use newly set state immediately

--- src/data_hub/holodeck/src/components/CollectionFilter.jsx
+++ src/data_hub/holodeck/src/components/CollectionFilter.jsx
@@ -10,21 +10,23 @@ export default class CollectionFilter extends React.Component {
   }
 
   handleSetFilter() {
-    this.setState({filter: !this.state.filter});
-    console.log(this.state);
-    if (this.state.filter) {
-      console.log('filter');
-      this.props.setCollectionFilter({
-        category: 'Environmental',
-        recommended_use: 'General Large Scale Geologic Information and Mapping'
-      });
-    } else {
-      this.props.setCollectionFilter({});
-    }
+    this.setState({filter: !this.state.filter}, () => {
+      console.log(this.state);
+      if (this.state.filter) {
+        console.log('filter');
+        this.props.setCollectionFilter({
+          category: 'Environmental',
+          recommended_use: 'General Large Scale Geologic Information and Mapping'
+        });
+      } else {
+        this.props.setCollectionFilter({});
+      }
+    });
   }
 
   render() {
     console.log(this.props);
+    console.log(this.state);
     return (
       <div>
         <button onClick={this.handleSetFilter}>filter</button>

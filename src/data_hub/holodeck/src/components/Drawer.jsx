f70a8aa6c37e052545b7bed8612465ec220914e7
Brez <smudgeservices@gmail.com>
2018-09-17 15:13:57 -0500

    drawer cleanup

--- src/data_hub/holodeck/src/components/Drawer.jsx
+++ src/data_hub/holodeck/src/components/Drawer.jsx
@@ -1,82 +1,96 @@
 import React from 'react';
-
-import tnrisLogo from '../images/tnris.png';
-import twdbLogo from '../images/twdb_splash.png';
+import {MDCDrawer} from "@material/drawer";
+import tnrisLogo from '../images/tnris_globe.png';
+import twdbLogo from '../images/twdb_splash_icon.png';
 
 export default class Drawer extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.closeDrawer = this.closeDrawer.bind(this);
+  }
+
+  componentDidMount() {
+    this.drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+  }
+
+  closeDrawer () {
+    this.drawer.open = false;
+  }
 
   render() {
     return (
       <div className="drawer-component">
         <aside className="mdc-drawer mdc-drawer--modal">
-          <div className="mdc-drawer__header">
-            <h3 className="mdc-drawer__title">Links</h3>
-            <h6 className="mdc-drawer__subtitle">to tnris.org</h6>
-          </div>
+
           <div className="mdc-drawer__content">
             <nav className="mdc-list">
               <a className="mdc-list-item" href="https://tnris.org/maps-and-data" aria-hidden="true">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">inbox</i>Maps &amp; Data
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">explore</i>Maps &amp; Data
               </a>
               <a className="mdc-list-item" href="https://tnris.org/geographic-information-office">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>Geographic Information Office
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">account_circle</i>Geographic Information Office
+              </a>
+              <a className="mdc-list-item" href="https://tnris.org/texas-imagery-service">
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">satellite</i>Texas Imagery Service
               </a>
 
-              <h3 className="mdc-drawer__title">Programs</h3>
-              <a href="https://tnris.org/stratmap">
-                <h6 className="mdc-list-group__subheader">Strategic Mapping Program</h6>
-                <h6 className="mdc-list-group__subheader">StratMap</h6>
+              <hr className="mdc-list-divider" />
+
+              <a className="mdc-list-item" href="https://tnris.org/stratmap">
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">map</i>Strategic Mapping Program
               </a>
               <a className="mdc-list-item" href="https://tnris.org/stratmap/stratmap-contracts">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>StratMap Contracts
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">edit</i>StratMap Contracts
               </a>
               <a className="mdc-list-item" href="https://tnris.org/stratmap/elevation-lidar">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>Elevation- Lidar
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">terrain</i>StratMap Elevation|Lidar
               </a>
               <a className="mdc-list-item" href="https://tnris.org/stratmap/orthoimagery">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>Orthoimagery
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">satellite</i>StratMap Orthoimagery
               </a>
 
-              <a href="https://tnris.org/research-distribution-center">
-                <h6 className="mdc-list-group__subheader">Research and Distribution Center (RDC)</h6>
+              <hr className="mdc-list-divider" />
+
+              <a className="mdc-list-item" href="https://tnris.org/research-distribution-center">
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">timeline</i>Research &amp; Distribution Center
               </a>
               <a className="mdc-list-item" href="https://tnris.org/research-distribution-center/historical-imagery-archive">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>Historical Imagery Archive
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">archive</i>Historical Imagery Archive
               </a>
               <a className="mdc-list-item" href="https://tnris.org/research-distribution-center/custom-maps">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>Custom Maps
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">place</i>Custom Maps
               </a>
 
-              <a href="https://tnris.org/education">
-                <h6 className="mdc-list-group__subheader">Education and Training</h6>
+              <hr className="mdc-list-divider" />
+
+              <a className="mdc-list-item" href="https://tnris.org/education">
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">school</i>Education and Training
               </a>
               <a className="mdc-list-item" href="https://tnris.org/education/gis-mentorship">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>GIS Mentorships
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">group</i>GIS Mentorships
               </a>
               <a className="mdc-list-item" href="https://tnris.org/education/purchasers">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>Purchaser Resources
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">account_balance</i>Purchaser Resources
               </a>
-
-              <a href="https://tnris.org/texas-imagery-service">
-                <h6 className="mdc-list-group__subheader">Texas Imagery Service</h6>
-              </a>
-
 
               <hr className="mdc-list-divider" />
 
               <a className="mdc-list-item" href="https://tnris.org/about">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">drafts</i>About
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">info</i>About
               </a>
               <a className="mdc-list-item" href="https://tnris.org/contact">
-                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">drafts</i>Contact Us
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">contact_mail</i>Contact Us
               </a>
               <a className="mdc-list-item" href="https://tnris.org">
-                <img src={tnrisLogo} className="mdc-list-item__graphic" aria-hidden="true" />tnris.org
+                <img src={tnrisLogo} className="mdc-list-item__graphic tnris-logo" alt="TNRIS Logo" aria-hidden="true" />tnris.org
               </a>
               <a className="mdc-list-item" href="http://www.twdb.texas.gov/">
-                <img src={twdbLogo} className="mdc-list-item__graphic" aria-hidden="true" />twdb.texas.gov
+                <img src={twdbLogo} className="mdc-list-item__graphic twdb-logo" alt="TWDB Logo" aria-hidden="true" />twdb.texas.gov
               </a>
-
+              <a className="mdc-list-item mdc-list-item--activated close-button" onClick={() => this.closeDrawer()} aria-selected="true">
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">close</i>Close
+              </a>
             </nav>
           </div>
         </aside>

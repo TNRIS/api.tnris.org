85bd901b7e506039a20c900564fd535b5b99b593
Brez <smudgeservices@gmail.com>
2018-09-18 14:14:10 -0500

    catalog card style

--- src/data_hub/holodeck/src/components/CatalogCard.jsx
+++ src/data_hub/holodeck/src/components/CatalogCard.jsx
@@ -20,13 +20,12 @@ export default class CatalogCard extends React.Component {
       <div className={cardClass}
            onClick={this.cardClicked}>
         <div
-          className='catalog-card__media mdc-card__media mdc-card__media--16-9'
+          className='catalog-card__media mdc-card__media mdc-card__media--square'
           style={{backgroundImage: `url(${this.props.collection.thumbnail_image})`}}
           alt="Dataset Thumbnail">
+          <p className='catalog-card__headline mdc-typography--body1'>{this.props.collection.name}</p>
         </div>
-        <p className='catalog-card__headline mdc-typography--body1'>{this.props.collection.name}</p>
-        <p className='catalog-card__body mdc-typography__body2'>{this.props.collection.short_description}</p>
-        <div className='catalog-card__footer'></div>
+
       </div>
     );
   }

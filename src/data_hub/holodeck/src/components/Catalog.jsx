a42fd861a9a49ec74abb9b757454140fa2aa2908
Brez <smudgeservices@gmail.com>
2018-09-20 16:00:16 -0500

    added import

--- src/data_hub/holodeck/src/components/Catalog.jsx
+++ src/data_hub/holodeck/src/components/Catalog.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import CatalogCardContainer from '../containers/CatalogCardContainer';
 import CollectionDialogContainer from '../containers/CollectionDialogContainer';
+import CollectionFilterContainer from '../containers/CollectionFilterContainer';
 import Drawer from './Drawer';
 import Footer from './Footer';
 import Header from './Header';

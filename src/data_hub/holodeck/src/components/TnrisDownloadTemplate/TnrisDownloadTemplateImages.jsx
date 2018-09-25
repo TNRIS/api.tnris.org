df7706da5f27997a72a832c9291e2b2f3267fb2c
John Haney <geojwh@gmail.com>
2018-09-25 11:14:02 -0500

    image carousel completed - maybe

--- src/data_hub/holodeck/src/components/TnrisDownloadTemplate/TnrisDownloadTemplateImages.jsx
+++ src/data_hub/holodeck/src/components/TnrisDownloadTemplate/TnrisDownloadTemplateImages.jsx
@@ -6,45 +6,84 @@ export default class TnrisDownloadTemplateImages extends React.Component {
   constructor(props) {
       super(props);
       console.log(this.props);
+      this.renderImages = this.renderImages.bind(this);
+  }
+
+  renderImages() {
+    Object.keys(this.props).map(key => {
+      console.log(key);
+      if (this.props[key]) {
+        console.log(key);
+        let imageDiv =
+          <div>
+            <img src={this.props[key]} alt='Image' />
+          </div>
+        ;
+      }
+    })
   }
 
   render() {
 
-    // let renderedImages =
-    //   this.props.forEach(function () {
-    //     if (this.props.natural) {
-    //       return (<div><img src={this.props.natural} alt='natural' /></div>);
-    //     }
-    //     if (this.props.overview) {
-    //       return (<div><img src={this.props.overview} alt='overview' /></div>);
-    //     }
-    //     if (this.props.urban) {
-    //       return (<div><img src={this.props.urban} alt='urban' /></div>);
-    //     }
-    //     else {
-    //       return (<div><h3>No images available.</h3></div>);
-    //     }
-    //   })
+    // const propArray = Object.keys(this.props);
+    // console.log(propArray);
+    // console.log(this.props);
+    const imageUrls = [];
+    Object.keys(this.props).map(key => this.props[key] !== null ? imageUrls.push(this.props[key]) : '')
+    console.log(imageUrls);
 
     return (
 
       <div className="tnris-download-template-images">
-
         <Carousel>
-
-          { if (this.props.natural) {
-            return <div><img src={this.props.natural} alt='natural' /></div>
+          {
+            imageUrls.map(url =>
+              <div key={url}>
+                <img src={url} alt='Image' />
+              </div>
+            )
           }
-
-          // <div><img src={this.props.overview} alt='overview' /></div>
-          //
-          // <div><img src={this.props.urban} alt='urban' /></div>
-
         </Carousel>
-
       </div>
 
-    );
-  }
+    )
 
-}
+
+    // for (var i in propArray) {
+    //   console.log(propArray.map);
+    //
+    //   console.log(this.props[propArray[i]]);
+    //
+    //   if (this.props[propArray[i]] !== null) {
+    //     return (
+    //       <div className="tnris-download-template-images">
+    //         <Carousel>
+    //           <div>
+    //             <img src={this.props[propArray[i]]} alt='Image' />
+    //           </div>
+    //         </Carousel>
+    //       </div>
+    //     );
+    //   } else {
+    //     return (
+    //       <div className="tnris-download-template-images">
+    //         <Carousel>
+    //           <div>
+    //             <h1></h1>
+    //           </div>
+    //         </Carousel>
+    //       </div>
+    //     )
+    //   };
+
+        // <div className="tnris-download-template-images">
+        //   <Carousel>
+        //     <div>
+        //       <img src={this.props[propArray[i]]} alt='Image' />
+        //     </div>
+        //   </Carousel>
+        // </div>
+
+
+    }
+  }

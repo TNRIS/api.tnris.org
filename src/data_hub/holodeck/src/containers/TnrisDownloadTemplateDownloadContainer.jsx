0fa2b349fbf3dd68c0055145cda764b75994d21f
Brez <smudgeservices@gmail.com>
2018-10-02 10:56:36 -0500

    download by resource_type

--- src/data_hub/holodeck/src/containers/TnrisDownloadTemplateDownloadContainer.jsx
+++ src/data_hub/holodeck/src/containers/TnrisDownloadTemplateDownloadContainer.jsx
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
-
+import { getResourceAreas } from '../selectors/resourceSelectors';
 import TnrisDownloadTemplateDownload from '../components/TnrisDownloadTemplate/TnrisDownloadTemplateDownload';
 
 const mapStateToProps = state => ({
   loadingResources: state.collections.loadingResources,
   errorResources: state.collections.errorResources,
-  selectedCollectionResources: state.collections.selectedCollectionResources
+  selectedCollectionResources: state.collections.selectedCollectionResources,
+  resourceAreas: getResourceAreas(state)
 });
 
 const mapDispatchToProps = (dispatch) => ({

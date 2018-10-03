6e99bfcfa60652cafec118c5caa6f1d37dfc4dda
John Haney <geojwh@gmail.com>
2018-10-03 09:27:49 -0500

    image carousel work; @media query for device width

--- src/data_hub/holodeck/src/components/TnrisDownloadTemplate/TnrisDownloadTemplateImages.jsx
+++ src/data_hub/holodeck/src/components/TnrisDownloadTemplate/TnrisDownloadTemplateImages.jsx
@@ -12,26 +12,43 @@ export default class TnrisDownloadTemplateImages extends React.Component {
 
     const imageUrls = [];
     Object.keys(this.props).map(key => this.props[key] !== null ? imageUrls.push(this.props[key]) : '')
+
     console.log(imageUrls);
 
     return (
 
       <div className="tnris-download-template-images">
-        <Carousel
-          autoPlay={true}
-          infiniteLoop={true}
-          emulateTouch
-          useKeyboardArrows={true}
-          transitionTime={1000}
-        >
+        <div className="desktop-lg">
+          <Carousel
+            autoPlay={true}
+            showThumbs={false}
+            infiniteLoop={true}
+            emulateTouch
+            useKeyboardArrows={true}
+            transitionTime={700}
+          >
+            {
+              imageUrls.map(url =>
+                <div key={url}>
+                  <img src={url} alt='' />
+                </div>
+              )
+            }
+          </Carousel>
+        </div>
+
+        <div className="mobile">
           {
             imageUrls.map(url =>
               <div key={url}>
                 <img src={url} alt='' />
+
+                  
+
               </div>
             )
           }
-        </Carousel>
+        </div>
       </div>
     )
   }

0044fbc1f7eb0ab552a9e0d075f5cb38785b18fd
Jason Kleinert <jason.kleinert@gmail.com>
2018-10-15 15:53:43 -0500

    updated model with areas view and areas.sql

--- src/data_hub/lcd/models.py
+++ src/data_hub/lcd/models.py
@@ -1160,6 +1160,7 @@ class CcrView(models.Model):
     def __str__(self):
         return self.name
 
+
 class AcdcView(models.Model):
     """
     Area Collection Data Connection view presents Resource table with joins
@@ -1192,6 +1193,7 @@ class AcdcView(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class RemView(models.Model):
     """
@@ -1238,3 +1240,32 @@ class RemView(models.Model):
 
     def __str__(self):
         return self.resource_id
+
+
+  class AreasView(models.Model):
+      """
+      Areas view presents resource table with aggregated collection_id
+      """
+
+      class Meta:
+          managed = False
+          db_table = "areas"
+          verbose_name = 'Areas'
+          verbose_name_plural = 'Areas'
+
+      area_type_id = models.UUIDField(
+          'Area Type ID',
+          primary_key=True
+      )
+      area_type_name = models.TextField(
+          'Area Type Name'
+      )
+      area_type = models.TextField(
+          'Area Type'
+      )
+      collections = models.TextField(
+          'Collections'
+      )
+
+      def __str__(self):
+          return str(self.area_type_id)

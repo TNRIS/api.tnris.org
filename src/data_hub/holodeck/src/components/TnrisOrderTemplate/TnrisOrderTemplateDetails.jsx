8477ede72d5724c2fc0a5b1f30b52460a9965489
john <geojwh@gmail.com>
2018-10-29 13:56:13 -0500

    order template cards completed

--- src/data_hub/holodeck/src/components/TnrisOrderTemplate/TnrisOrderTemplateDetails.jsx
+++ src/data_hub/holodeck/src/components/TnrisOrderTemplate/TnrisOrderTemplateDetails.jsx
@@ -3,19 +3,27 @@ import React from 'react';
 import Description from '../DialogTemplateListItems/Description'
 import LidarBlurb from '../DialogTemplateListItems/LidarBlurb'
 import Metadata from '../DialogTemplateListItems/Metadata'
+import Services from '../DialogTemplateListItems/Services'
+import Supplementals from '../DialogTemplateListItems/Supplementals'
 
 import ContactContainer from '../../containers/ContactContainer';
-// import OrderTnrisDataFormContainer from '../../containers/OrderTnrisDataFormContainer';
+import OrderTnrisDataFormContainer from '../../containers/OrderTnrisDataFormContainer';
 
 export default class TnrisDownloadTemplateDetails extends React.Component {
 
   render() {
     const lidarCard = this.props.collection.category === 'Lidar' ? <LidarBlurb /> : "";
+    const supplementalDownloadsCard = (this.props.collection.tile_index_url ||
+                                        this.props.collection.supplemental_report_url ||
+                                        this.props.collection.lidar_breaklines_url) ?
+                                        <Supplementals collection={this.props.collection} /> : "";
+    const servicesCard = this.props.collection.wms_link ? <Services collection={this.props.collection} /> : "";
 
     return (
       <div className='tnris-download-template-details'>
         <Metadata collection={this.props.collection} />
-
+        {supplementalDownloadsCard}
+        {servicesCard}
         <Description collection={this.props.collection} />
         {lidarCard}
         <div className="template-content-div">

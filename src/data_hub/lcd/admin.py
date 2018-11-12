571a6ad9ac46b0c3ff8261a2b40d9ea47b34b2fe
Brez <smudgeservices@gmail.com>
2018-11-12 10:48:50 -0600

    admin collection and resource names match display name in frontend

--- src/data_hub/lcd/admin.py
+++ src/data_hub/lcd/admin.py
@@ -115,12 +115,22 @@ class CollectionAdmin(admin.ModelAdmin):
     inlines = [ImageInlineAdmin]
     ordering = ('name',)
     list_display = (
-        'name', 'collection_id', 'last_modified', 'public'
+        'disp_name', 'collection_id', 'last_modified', 'public'
     )
-    search_fields = ('name', 'collection_id')
+    search_fields = ('disp_name', 'collection_id')
     list_filter = (
         'public', 'template_type_id'
     )
+
+    # set aside acqusition_date year for list display
+    def disp_name(self, collection):
+        if collection.acquisition_date is not None and collection.template_type_id.template != 'outside-entity':
+            year = collection.acquisition_date.split("-")[0] + " "
+        else:
+            year = ""
+        return year + collection.name
+    disp_name.short_description = 'Collection Display Name'
+
     # remove default action 'delete_selected' so s3 files will be deleted by the
     # model's overridden delete method. also so user permissions don't have to be
     # handled. **No logical scenario calls for Collections to be bulk deleted
@@ -203,14 +213,23 @@ class ResourceAdmin(admin.ModelAdmin):
 
     ordering = ('collection_id',)
     list_display = (
-        'collection_id', 'area_type_id', 'resource', 'resource_type_id', 'last_modified'
+        'disp_name', 'area_type_id', 'resource', 'resource_type_id', 'last_modified'
     )
-    search_fields = ('collection_id__name', 'area_type_id__area_type_name', 'resource', 'resource_type_id__resource_type_name')
+    search_fields = ('disp_name', 'collection_id__name', 'area_type_id__area_type_name', 'resource', 'resource_type_id__resource_type_name')
     list_filter = (
         'collection_id',
         'area_type_id',
         'resource_type_id'
     )
+
+    # set aside acqusition_date year for list display
+    def disp_name(self, resource):
+        if resource.collection_id.acquisition_date is not None and resource.collection_id.template_type_id.template != 'outside-entity':
+            year = resource.collection_id.acquisition_date.split("-")[0] + " "
+        else:
+            year = ""
+        return year + resource.collection_id.name
+    disp_name.short_description = 'Collection Display Name'
 
     # override the /resource/add/ form
     def add_view(self,request,extra_context=None):

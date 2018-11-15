c842a133119287529b2932afa7df34c3e4bfcf7f
Jason Kleinert <jason.kleinert@gmail.com>
2018-11-15 08:46:31 -0600

    updated map filtering based on a user drawn rectangle

--- src/data_hub/holodeck/src/actions/collectionFilterMapActions.js
+++ src/data_hub/holodeck/src/actions/collectionFilterMapActions.js
@@ -1,8 +1,18 @@
 import {
+  SET_COLLECTION_FILTER_MAP_AOI,
   SET_COLLECTION_FILTER_MAP_CENTER,
   SET_COLLECTION_FILTER_MAP_FILTER,
   SET_COLLECTION_FILTER_MAP_ZOOM,
 } from '../constants/collectionFilterMapActionTypes';
+
+export const setCollectionFilterMapAoi = (collectionFilterMapAoi) => {
+  return (dispatch) => {
+    dispatch({
+      type: SET_COLLECTION_FILTER_MAP_AOI,
+      payload: { collectionFilterMapAoi }
+    })
+  }
+};
 
 export const setCollectionFilterMapCenter = (collectionFilterMapCenter) => {
   return (dispatch) => {

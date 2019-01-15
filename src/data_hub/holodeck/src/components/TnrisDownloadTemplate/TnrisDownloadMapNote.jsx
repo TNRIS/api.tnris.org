17c5c69e6e69046bfb2aad522f45ebf7798f52c0
Brez <smudgeservices@gmail.com>
2019-01-15 15:00:13 -0600

    order cart item sass for mobile. modal goes full screen on mobile phone. adjusted download map note.

--- src/data_hub/holodeck/src/components/TnrisDownloadTemplate/TnrisDownloadMapNote.jsx
+++ src/data_hub/holodeck/src/components/TnrisDownloadTemplate/TnrisDownloadMapNote.jsx
@@ -11,9 +11,9 @@ export default class TnrisDownloadMapNote extends React.Component {
   render() {
     const noteContent = this.state.noteHover ? (
       <p>
-        Every dataset is available for order directly from TNRIS. Click the Order tab if the quantity of data needed is too large to download.
+        Every dataset (excluding External Link availability) is available for order directly from TNRIS. Click the Order tab if the quantity of data needed is too large to download.
         <br />
-        Every dataset is publically available via an AWS S3 bucket and can be accessed using the AWS CLI for bulk and programmatic downloads. Click the Contact tab to inquire with TNRIS about details related to S3 bulk access.
+        Every downloadable dataset is publically available via an AWS S3 bucket and can be accessed using the AWS CLI for bulk and programmatic downloads. Click the Contact tab to inquire with TNRIS about details related to S3 bulk access.
       </p>
     ) : (
       <i className="material-icons">toys</i>

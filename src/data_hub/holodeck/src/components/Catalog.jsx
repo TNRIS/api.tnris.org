0e842e0b6f7c6a31cabe95be73655a66af3df70e
Jason Kleinert <jason.kleinert@gmail.com>
2019-01-18 15:49:36 -0600

    added downshift

--- src/data_hub/holodeck/src/components/Catalog.jsx
+++ src/data_hub/holodeck/src/components/Catalog.jsx
@@ -150,7 +150,7 @@ export default class Catalog extends React.Component {
     return (
       <div className="catalog-component">
 
-        <OrderCartDialogContainer />
+        {/*<OrderCartDialogContainer />*/}
         <CollectionFilterMapDialogContainer />
 
         <ToolDrawerContainer
@@ -183,7 +183,7 @@ export default class Catalog extends React.Component {
               <ul className="mdc-layout-grid__inner">
                 {this.props.visibleCollections ? this.props.visibleCollections.map(collectionId =>
                   <li
-                    className="mdc-layout-grid__cell mdc-layout-grid__cell--span-2"
+                    className="mdc-layout-grid__cell mdc-layout-grid__cell--span-3"
                     key={collectionId}>
                     <CatalogCardContainer
                       collection={this.props.collections[collectionId]}
--- src/data_hub/holodeck/src/components/CollectionSearcher.jsx
+++ src/data_hub/holodeck/src/components/CollectionSearcher.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { MDCRipple } from '@material/ripple';
+import Downshift from 'downshift';
+import {MDCList} from '@material/list';
+import {MDCMenu} from '@material/menu';
+import {MDCMenuSurface} from '@material/menu-surface';
+import {MDCRipple} from '@material/ripple';
 import { MDCTextField } from '@material/textfield';
 import { Redirect } from 'react-router';
+
+const searchFieldBackgroundColor = '#efefef';
 
 export default class CollectionSearcher extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      badUrlFlag: false
+      badUrlFlag: false,
+      searchFieldValue: '',
+      showSuggestionList: false,
+      selectValue: 'value'
     }
     this.handleSearch = this.handleSearch.bind(this);
-    this.clearSearch = this.clearSearch.bind(this);
+    this.handleClearSearch = this.handleClearSearch.bind(this);
+    this.updateUrl = this.updateUrl.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
   }
 
   componentDidMount() {
     this.searchField = new MDCTextField(document.querySelector('.search-component'));
+    this.searchFieldInput = document.querySelector('.mdc-text-field__input');
+    this.searchFieldInput.style.backgroundColor = searchFieldBackgroundColor;
+    // this.list = new MDCList(document.querySelector('.mdc-list'));
+    // this.list.singleSelection = true;
+    // this.listItemRipples = this.list.listElements.map((listItemEl) => new MDCRipple(listItemEl));
 
     // on component mount, check the URl to apply any necessary filters
     // first, check if url has a 'filters' parameter
@@ -26,6 +43,7 @@ export default class CollectionSearcher extends React.Component {
         if (Object.keys(allFilters).includes('search')) {
           // third, apply search text and then populate the input box
           this.props.setCollectionSearchQuery(allFilters.search);
+          this.props.setCollectionSearchSuggestionsQuery(allFilters.search);
           document.querySelector('#search-collections').value = allFilters.search;
         }
       } catch (e) {
@@ -38,19 +56,17 @@ export default class CollectionSearcher extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.collectionSearchQuery === "") {
-      document.getElementById('search-collections').value = '';
-    }
+    // if (nextProps.collectionSearchQuery === "") {
+    //   document.getElementById('search-collections').value = '';
+    // }
   }
 
-  handleSearch() {
-    this.props.setCollectionSearchQuery(this.searchField.value);
-
+  updateUrl(searchFieldValue) {
     // update URL to reflect new search change
     const prevFilter = this.props.history.location.pathname.includes('/catalog/') ?
                        JSON.parse(decodeURIComponent(this.props.history.location.pathname.replace('/catalog/', '')))
                        : {};
-    const filterObj = {...prevFilter, search: this.searchField.value};
+    const filterObj = {...prevFilter, search: searchFieldValue};
     // if search is empty, remove from the url
     if (filterObj['search'].length === 0) {
       delete filterObj['search'];
@@ -60,49 +76,244 @@ export default class CollectionSearcher extends React.Component {
     Object.keys(filterObj).length === 0 ? this.props.setUrl('/', this.props.history) : this.props.setUrl('/catalog/' + encodeURIComponent(filterString), this.props.history);
   }
 
-  clearSearch() {
-    if (this.searchField.value) {
-      document.getElementById("search-collections").value = "";
-      this.handleSearch();
+  handleBlur() {
+    this.searchField.foundation_.adapter_.removeClass('shadowy');
+    this.searchFieldInput.style.backgroundColor = searchFieldBackgroundColor;
+    // this.list.foundation_.adapter_.focusItemAtIndex(1);
+    // this.setState({showSuggestionList: false});
+  }
+
+  handleFocus() {
+    this.searchField.foundation_.adapter_.addClass('shadowy');
+    this.searchFieldInput.style.backgroundColor = 'white';
+  }
+
+  // handleSearch(e) {
+  //   try {
+  //     if (this.searchField.value) {
+  //       // first set the collectionSearchSuggestionsQuery in the app state then set the
+  //       // local state for the clear button handling to show the suggestion list
+  //       this.props.setCollectionSearchSuggestionsQuery(this.searchField.value);
+  //       this.setState({
+  //         // searchFieldValue: this.searchField.value,
+  //         showSuggestionList: true
+  //       });
+  //       // we check if the user pressed the enter or escape key
+  //       if (e.keyCode === 13) { // they pressed enter, so fire the search
+  //           e.preventDefault(); // ensure it is only our code that is run
+  //           this.props.setCollectionSearchQuery(this.searchField.value);
+  //           this.updateUrl(this.searchField.value);
+  //           this.setState({showSuggestionList: false});
+  //           this.searchFieldInput.blur();
+  //       } else if (e.keyCode === 27) { // they pressed escape, so drop focus
+  //         // this.searchFieldInput.blur();
+  //       }
+  //     } else {
+  //       if (e.keyCode === 13) { // they pressed enter, so fire the search
+  //         this.props.setCollectionSearchQuery('');
+  //         this.props.setCollectionSearchSuggestionsQuery('');
+  //         this.updateUrl('');
+  //         this.setState({
+  //           // searchFieldValue: null,
+  //           showSuggestionList: false
+  //         });
+  //         this.searchFieldInput.blur();
+  //       }
+  //       if (e.keyCode === 27) { // they pressed escape, so drop focus
+  //         this.searchFieldInput.blur();
+  //         this.setState({
+  //           // searchFieldValue: null,
+  //           showSuggestionList: false
+  //         });
+  //       }
+  //       this.props.setCollectionSearchSuggestionsQuery('');
+  //       this.setState({
+  //         // searchFieldValue: null,
+  //         showSuggestionList: false
+  //       });
+  //     }
+  //   } catch(e) {
+  //     console.log(e);
+  //   }
+  // }
+  //
+  handleSearch(e) {
+    try {
+      console.log(e);
+      if (e.target.value) {
+        console.log(e.target.value);
+        // first set the collectionSearchSuggestionsQuery in the app state then set the
+        // local state for the clear button handling to show the suggestion list
+        this.props.setCollectionSearchSuggestionsQuery(e.target.value);
+        this.setState({
+          // searchFieldValue: this.searchField.value,
+          showSuggestionList: true
+        });
+        // we check if the user pressed the enter or escape key
+        if (e.keyCode === 13) { // they pressed enter, so fire the search
+            e.preventDefault(); // ensure it is only our code that is run
+            this.props.setCollectionSearchQuery(e.target.value);
+            this.updateUrl(e.target.value);
+            this.setState({showSuggestionList: false});
+        } else if (e.keyCode === 27) { // they pressed escape, so drop focus
+          // this.searchFieldInput.blur();
+        }
+      } else {
+        if (e.keyCode === 13) { // they pressed enter, so fire the search
+          this.props.setCollectionSearchQuery('');
+          this.props.setCollectionSearchSuggestionsQuery('');
+          this.updateUrl('');
+          this.setState({
+            // searchFieldValue: null,
+            showSuggestionList: false
+          });
+        }
+        if (e.keyCode === 27) { // they pressed escape, so drop focus
+          this.setState({
+            // searchFieldValue: null,
+            showSuggestionList: false
+          });
+        }
+        this.props.setCollectionSearchSuggestionsQuery('');
+        this.setState({
+          // searchFieldValue: null,
+          showSuggestionList: false
+        });
+      }
+    } catch(e) {
+      console.log(e);
     }
   }
+
+  handleClearSearch(e) {
+    try {
+      if (this.searchField.value) {
+        // document.getElementById("search-collections").value = '';
+        this.props.setCollectionSearchQuery('');
+        this.props.setCollectionSearchSuggestionsQuery('');
+        this.updateUrl('');
+        this.setState({
+          // searchFieldValue: null,
+          showSuggestionList: false
+        });
+        this.searchFieldInput.focus();
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  // <div
+  //   id="searchparent"
+  //   className={`search-component mdc-text-field mdc-text-field--fullwidth
+  //     mdc-text-field--with-leading-icon mdc-text-field--with-trailing-icon mdc-menu-surface--anchor`}>
+  //   <i id="search-icon" className="material-icons mdc-text-field__icon">search</i>
+  //   <input className='mdc-text-field__input'
+  //          type='search'
+  //          id='search-collections'
+  //          name='q'
+  //          onClick={this.click}
+  //          onKeyUp={this.handleSearch}
+  //          onFocus={this.handleFocus}
+  //          onBlur={this.handleBlur}
+  //          placeholder='Search'
+  //          aria-label='Search data collections'
+  //          tabIndex="0"
+  //          autoComplete="off">
+  //   </input>
+  //   {this.props.collectionSearchSuggestionsQuery ?
+  //     <button
+  //       id='clear-icon'
+  //       className="mdc-icon-button material-icons mdc-text-field__icon"
+  //       tabIndex="0"
+  //       onClick={this.handleClearSearch}>
+  //       clear
+  //     </button> : ''}
+  //     {this.state.showSuggestionList && this.props.collectionSearchSuggestions.length > 0 ?
+  //       <div className="suggestion-list--container">
+  //         <ul id="suggestion-list" className="mdc-list" role="listbox">
+  //         {this.props.collectionSearchSuggestions.slice(0,5).map(suggestion =>
+  //           <li className="mdc-list-item" key={suggestion} role="option">
+  //             <span className="mdc-list-item__text">{suggestion}</span>
+  //           </li>
+  //         )}
+  //         </ul>
+  //       </div> : ''}
+  // </div>
 
   render() {
     if (this.state.badUrlFlag) {
       return <Redirect to='/404' />;
     }
 
-    // const clearDiv = <div id="clear-search" onClick={this.clearSearch} title="Clear search">
-    //                    <i id="clear-icon" className="material-icons">clear</i>
-    //                  </div>
-
+    // if (this.searchField) {
+    //   if (document.querySelector('.mdc-text-field__input') === document.activeElement) {
+    //     this.searchField.foundation_.adapter_.addClass('shadowy');
+    //     this.searchFieldInput.style.backgroundColor = 'white';
+    //   } else {
+    //     this.searchField.foundation_.adapter_.removeClass('shadowy');
+    //     this.searchFieldInput.style.backgroundColor = 'grey';
+    //   }
+    // }
+    console.log(this.state);
+    console.log(this.props);
     return (
-      <div
-        id="searchparent"
-        className="search-component mdc-text-field mdc-text-field--fullwidth mdc-text-field--with-leading-icon mdc-text-field--with-trailing-icon">
-        {/*{
-          this.props.collectionSearchQuery ? clearDiv : ''
-        }*/}
-        <i id="search-icon" className="material-icons mdc-text-field__icon">search</i>
-        <input className='mdc-text-field__input'
-               type='search'
-               id='search-collections'
-               name='q'
-               onChange={this.handleSearch}
-               placeholder='Search'
-               aria-label='Search data collections'
-               tabIndex="0">
-        </input>
-        {this.props.collectionSearchQuery ?
-          <i
-            id='clear-icon'
-            className="material-icons mdc-text-field__icon"
-            tabIndex="0"
-            role="button"
-            onClick={this.clearSearch}>
-            clear
-          </i> : ''}
-      </div>
+      <Downshift
+        onChange={selection => alert(`You selected ${selection}`)}
+        itemToString={item => (item ? item.value : '')}
+      >
+        {({
+          getInputProps,
+          getItemProps,
+          getLabelProps,
+          getMenuProps,
+          isOpen,
+          inputValue,
+          reset,
+          highlightedIndex,
+          selectedItem,
+        }) => (
+          <div
+            id="searchparent"
+            className={`search-component mdc-text-field mdc-text-field--fullwidth
+              mdc-text-field--with-leading-icon mdc-text-field--with-trailing-icon mdc-menu-surface--anchor`}>
+            <i id="search-icon" className="material-icons mdc-text-field__icon">search</i>
+            <input
+              className="downshift-input mdc-text-field__input"
+              id="search-collections"
+              {...getInputProps({
+              placeholder: "Search",
+              onKeyUp: this.handleSearch,
+              onFocus: this.handleFocus,
+              onBlur: this.handleBlur
+            })}>
+            </input>
+            {this.props.collectionSearchSuggestionsQuery ?
+              <button
+                id='clear-icon'
+                className="mdc-icon-button material-icons mdc-text-field__icon"
+                tabIndex="0"
+                onClick={this.handleClearSearch}>
+                clear
+              </button> : ''}
+              {this.state.showSuggestionList && this.props.collectionSearchSuggestions.length > 0 ?
+
+                <ul className="suggestion-list downshift-dropdown mdc-list">
+                  {this.props.collectionSearchSuggestions.slice(0,5).map((item, index) =>
+                    <li
+                      className="dropdown-item mdc-list-item"
+                      {...getItemProps({ key: index, index, item })}
+                            style={{
+                              backgroundColor: highlightedIndex === index ? 'lightgray' : 'white',
+                              fontWeight: selectedItem === item ? 'bold' : 'normal',
+                            }}>
+                      {item}
+                    </li>
+                  )}
+                </ul> : ''}
+          </div>
+        )}
+      </Downshift>
     );
   }
 }

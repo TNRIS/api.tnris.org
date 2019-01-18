01e6977c468eae03230f05897dc02075a000e278
John Haney <geojwh@gmail.com>
2019-01-18 14:28:10 -0600

    there is only light and dark...
    comment out old themeOptions list constructor; new one with only light and dark. issue #48

--- src/data_hub/holodeck/src/components/ThemeChooser.jsx
+++ src/data_hub/holodeck/src/components/ThemeChooser.jsx
@@ -4,7 +4,8 @@ class ThemeChooser extends Component {
   constructor(props) {
       super(props);
       this.setColorTheme = this.setColorTheme.bind(this);
-      this.themeOptions = ['light', 'dark', 'earth', 'fuego', 'vaporwave', 'america', 'hulk', 'relax'];
+      // this.themeOptions = ['light', 'dark', 'earth', 'fuego', 'vaporwave', 'america', 'hulk', 'relax'];
+      this.themeOptions = ['light', 'dark'];
   }
 
   componentDidMount() {

1837e818fe59c32495805c313d5312ca93cbc6b6
Brez <smudgeservices@gmail.com>
2019-02-11 09:05:57 -0600

    removed history remnants

--- src/data_hub/holodeck/src/components/CollectionFilter.jsx
+++ src/data_hub/holodeck/src/components/CollectionFilter.jsx
@@ -169,7 +169,7 @@ export default class CollectionFilter extends React.Component {
     }
     const filterString = JSON.stringify(filterObj);
     // if empty filter settings, use the base home url instead of the filter url
-    Object.keys(filterObj).length === 0 ? this.props.setUrl('/', this.props.history) : this.props.setUrl('/catalog/' + encodeURIComponent(filterString), this.props.history);
+    Object.keys(filterObj).length === 0 ? this.props.setUrl('/') : this.props.setUrl('/catalog/' + encodeURIComponent(filterString));
     // log filter change in store
     Object.keys(filterObj).length === 0 ? this.props.logFilterChange('/') : this.props.logFilterChange('/catalog/' + encodeURIComponent(filterString));
   }

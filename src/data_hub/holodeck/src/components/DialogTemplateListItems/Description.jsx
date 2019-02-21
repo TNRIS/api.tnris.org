a49e3b60b4a4fa4dba8158f69777a0d5c4f5d024
John Haney <geojwh@gmail.com>
2019-02-21 12:23:17 -0600

    issue #84
    oe services collapsed to button; wiki api used for oe descriptions

--- src/data_hub/holodeck/src/components/DialogTemplateListItems/Description.jsx
+++ src/data_hub/holodeck/src/components/DialogTemplateListItems/Description.jsx
@@ -3,26 +3,51 @@ import React from 'react';
 
 export default class Description extends React.Component {
 
-  render() {
+  constructor(props) {
+    super(props)
 
-    let wikiName = this.props.collection.template === 'outside-entity' && this.props.collection.source_name.includes(' ') ? this.props.collection.source_name.split(' ').join('_') : '';
-    let wikiRequestUrl = wikiName !== '' ? `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='${wikiName}'` : '';
+    this.state = {
+      wikiExtract: ''
+    }
+  }
+
+  componentDidMount() {
+    /////////////////////
+    // wiki api stuff
+    // if template is outside entity, assign source_name prop to variable
+    const wikiName = this.props.collection.template === 'outside-entity' ? this.props.collection.source_name : '';
+    // Unauthenticated CORS requests may be made from any origin by setting the "origin" request parameter to "*"
+    const wikiUrl = `https://en.wikipedia.org/w/api.php?&origin=*&action=query&prop=extracts&format=json&redirects=1&exintro=&titles=${wikiName}`;
+    // reasign this to self so state isn't confused with promise in fetch
+    const self = this;
 
     if (wikiName !== '') {
-      fetch(wikiRequestUrl)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(outsideJson) {
-          console.log(JSON.stringify(outsideJson));
-        });
+      fetch(wikiUrl).then(function(response) {
+        if (!response.ok) {
+          throw new Error('Could not retrieve Wikipedia response.');
+        }
+        return response.json();
+      })
+      .then(function(data) {
+        const wikiExtract = data.query.pages[Object.keys(data.query.pages)[0]].extract;
+        self.setState({wikiExtract:wikiExtract});
+      })
     }
+    // end wiki api stuff
+    /////////////////////
+  }
 
-    console.log(wikiName);
-    console.log(wikiRequestUrl);
+  render() {
+
+    const createMarkup = () => {
+      return {__html: this.state.wikiExtract};
+    }
 
     return (
       <div className="template-content-div">
+        {
+          <div dangerouslySetInnerHTML={createMarkup()} />
+        }
         <p>
           {this.props.collection.description}
         </p>

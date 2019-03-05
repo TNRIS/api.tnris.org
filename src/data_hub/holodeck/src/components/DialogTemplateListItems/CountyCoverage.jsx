85f7b79119789000fd951a12e57a92e6fbffb8f8
Brez <smudgeservices@gmail.com>
2019-03-05 10:19:41 -0600

    updated coverage map to be consistent with download map layers and styling. added hover layer and click for county name popup

--- src/data_hub/holodeck/src/components/DialogTemplateListItems/CountyCoverage.jsx
+++ src/data_hub/holodeck/src/components/DialogTemplateListItems/CountyCoverage.jsx
@@ -32,8 +32,6 @@ export default class CountyCoverage extends React.Component {
     this.map = map;
     // add those controls!
     map.addControl(new mapboxgl.NavigationControl(), 'top-left');
-    // disable map zoom when using scroll
-    map.scrollZoom.disable();
 
     const re = new RegExp(", ", 'g');
     const quotedCounties = this.props.counties.replace(re, "','");
@@ -88,6 +86,19 @@ export default class CountyCoverage extends React.Component {
                   'fill-outline-color': styles[texter]
                 }
             });
+            // add the polygon area_type hover layer. wired below to toggle on hover
+            map.addLayer({
+                id: 'county-polygons-hover',
+                'type': 'fill',
+                'source': 'county-polygons-source',
+                'source-layer': 'layer0',
+                'paint': {
+                  'fill-color': styles[filler],
+                  'fill-opacity': .7,
+                  'fill-outline-color': styles[texter]
+                },
+                'filter': ['==', 'area_type_name', '']
+            }, 'county-polygons');
             // add the labels layer for the area_type polygons
             // map.addLayer({
             //     id: 'county-polygons-labels',
@@ -102,6 +113,29 @@ export default class CountyCoverage extends React.Component {
             //       "text-color": styles[texter]
             //     }
             // });
+
+            map.on('click', 'county-polygons', function (e) {
+              // console.log(e.lngLat);
+              const clickedAreaName = e.features[0].properties.area_type_name;
+              // create popup with constructed content string
+              new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(`<strong>${clickedAreaName} County</strong>`)
+                .addTo(map);
+            });
+
+            // Change the cursor to a pointer when it enters a feature in the 'area_type' layer
+            // Also, toggle the hover layer with a filter based on the cursor
+            map.on('mousemove', 'county-polygons', function (e) {
+              map.getCanvas().style.cursor = 'pointer';
+              map.setFilter('county-polygons-hover', ['==', 'area_type_name', e.features[0].properties.area_type_name]);
+            });
+            // Undo the cursor pointer when it leaves a feature in the 'area_type' layer
+            // Also, untoggle the hover layer with a filter
+            map.on('mouseleave', 'county-polygons', function () {
+              map.getCanvas().style.cursor = '';
+              map.setFilter('county-polygons-hover', ['==', 'area_type_name', '']);
+            });
         }, 500);
     });
   }
@@ -109,9 +143,6 @@ export default class CountyCoverage extends React.Component {
   render() {
     return (
       <div className="county-coverage-component">
-        <p className="mdc-typography--body2">
-          This dataset either partly or completely covers the counties: <strong>{this.props.counties}</strong>
-        </p>
         <div id='county-coverage-map'></div>
       </div>
     )

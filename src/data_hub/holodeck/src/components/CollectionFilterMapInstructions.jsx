617a094dc36f949e4ae0ea2ce5573a5d10a8dbfc
Jason Kleinert <jason.kleinert@gmail.com>
2019-03-08 15:27:47 -0600

    collectionFilterMap instructions and  sass tweak

--- src/data_hub/holodeck/src/components/CollectionFilterMapInstructions.jsx
+++ src/data_hub/holodeck/src/components/CollectionFilterMapInstructions.jsx
@@ -22,7 +22,7 @@ export default class CollectionFilterMapInstructions extends React.Component {
         <div className="mdc-typography--body1 instruction-paragraph">
           Use the 'polygon tool' &nbsp;
           <div id="instruction-polygon-icon" className="mapbox-gl-draw_polygon"></div>&nbsp;
-          in the top left corner  of the map to identify a geographic area to filter datasets.
+          in the top left corner of the map to identify a geographic area.
         </div>
         <div className="mdc-typography--body1 instruction-paragraph">
           Single click to begin drawing, move cursor to draw a filter extent, single click to finish drawing.

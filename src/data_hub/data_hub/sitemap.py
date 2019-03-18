3915a0decef7c96293a0cce60c5cef0971f4f164
Brez <smudgeservices@gmail.com>
2019-03-18 15:13:53 -0500

    expanded sitemap deets

--- src/data_hub/data_hub/sitemap.py
+++ src/data_hub/data_hub/sitemap.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from lcd.models import CcrView
 from lore.models import ChcView
 from itertools import chain
+import datetime
 
 
 class StaticSitemap(Sitemap):
@@ -17,6 +18,12 @@ class StaticSitemap(Sitemap):
     def location(self, item):
         return reverse(item)
 
+    def lastmod(self, obj):
+        return datetime.datetime.today()
+
+    def changefreq(self, obj):
+        return 'monthly'
+
 
 class CollectionSitemap(Sitemap):
 
@@ -25,3 +32,9 @@ class CollectionSitemap(Sitemap):
 
     def location(self, item):
         return reverse('collection', args=[item.collection_id])
+
+    def lastmod(self, obj):
+        return datetime.datetime.today()
+
+    def changefreq(self, obj):
+        return 'monthly'

bda13a2f58c9e5353231682f5dc505a352c4fcdd
Brez <smudgeservices@gmail.com>
2019-03-19 14:56:28 -0500

    use seo token from param store on deployment

--- docker-entrypoint.d/00-start-app.sh
+++ docker-entrypoint.d/00-start-app.sh
@@ -6,6 +6,7 @@ export DB_USER=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/data_tnri
 export DB_PASSWORD=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/data_tnris_org/db_password --with-decryption --region us-east-1 --output text | awk '{print $6}'`
 export DB_HOST=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/data_tnris_org/db_host --with-decryption --region us-east-1 --output text | awk '{print $6}'`
 export DB_PORT=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/data_tnris_org/db_port --with-decryption --region us-east-1 --output text | awk '{print $6}'`
+export SEO_TOKEN=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/data_tnris_org/seo_token --with-decryption --region us-east-1 --output text | awk '{print $6}'`
 
 eval /src/data_hub/manage.py collectstatic --noinput "$@"
 eval /usr/bin/supervisord "$@"

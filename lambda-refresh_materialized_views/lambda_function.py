f7dc8cfc6379cb4004f9c70388ef1e19bd09cb69
Brez <smudgeservices@gmail.com>
2019-03-29 14:57:44 -0500

    refresh materialized views lambda

---
+++ lambda-refresh_materialized_views/lambda_function.py
@@ -0,0 +1,36 @@
+# --------------- IMPORTS ---------------
+import os, psycopg2
+
+# Database Connection Info
+database = os.environ.get('DB_NAME')
+username = os.environ.get('DB_USER')
+password = os.environ.get('DB_PASSWORD')
+host = os.environ.get('DB_HOST')
+port = os.environ.get('DB_PORT')
+
+conn_string = "dbname='%s' user='%s' host='%s' password='%s' port='%s'" % (database, username, host, password, port)
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    print(event)
+    # connect to database
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+
+    queries = [
+        "REFRESH MATERIALIZED VIEW collection_catalog_record with DATA;",
+        "REFRESH MATERIALIZED VIEW compiled_historical_collection with DATA;"
+    ]
+
+    for q in queries:
+        print(q)
+        cur.execute(q)
+        conn.commit()
+
+    cur.close()
+    conn.close()
+    print("that's all folks!!")
+
+if __name__ == '__main__':
+    lambda_handler(event='event', context='context')

14a88701f62cd8f543712b0b2983af30510814b3
Marlboro Blend No. 27 <adam.breznicky@twdb.texas.gov>
2020-01-22 11:52:32 -0600

    update database utility function lambda

---
+++ lambda-update_database_record_utility/lambda_function.py
@@ -0,0 +1,42 @@
+# --------------- IMPORTS ---------------
+import os, psycopg2
+
+# Database Connection Info
+database = os.environ.get('DB_NAME')
+username = os.environ.get('DB_USER')
+password = os.environ.get('DB_PASSWORD')
+host = os.environ.get('DB_HOST')
+port = os.environ.get('DB_PORT')
+
+conn_string = "dbname='%s' user='%s' host='%s' password='%s' port='%s'" % (database, username, host, password, port)
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    print(event)
+    table = event['table']
+    field = event['field']
+    value = event['value']
+    where = event['where']
+    # connect to database
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+
+    if isinstance(value, str):
+        value = "'" + value + "'"
+
+    q = "UPDATE %s SET %s = %s;" % (table, field, value)
+
+    if where in event.keys() and where != '':
+        q = q.replace(";", " WHERE %s;" % where)
+
+    print(q)
+    cur.execute(q)
+    conn.commit()
+
+    cur.close()
+    conn.close()
+    print("that's all folks!!")
+
+if __name__ == '__main__':
+    lambda_handler(event='event', context='context')

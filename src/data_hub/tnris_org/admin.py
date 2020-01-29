00d270b5c0cdab0ce6cae0b3ff58c5e1c8567e80
jwhaney <john.haney@twdb.texas.gov>
2020-01-29 09:54:49 -0600

    removed tiny_preview from tnris_org documents
    issue #64

--- src/data_hub/tnris_org/admin.py
+++ src/data_hub/tnris_org/admin.py
@@ -72,7 +72,7 @@ class TnrisDocumentAdmin(admin.ModelAdmin):
     model = TnrisDocument
     form = DocumentForm
     ordering = ('document_name',)
-    list_display = ('document_name', 'document_url_link', 'tiny_preview', 'created')
+    list_display = ('document_name', 'document_url_link', 'created')
     search_fields = ('document_name', 'document_url')
 
     def document_url_link(self, obj):
@@ -94,19 +94,6 @@ class TnrisDocumentAdmin(admin.ModelAdmin):
             htmlId,
             htmlId,
             obj.document_url
-        )
-
-    def tiny_preview(self, obj):
-        # tiny preview of .pdf files errors/breaks on page load when src is cached.
-        # this means all tiny previews display on hard reload, but .pdfs don't on
-        # normal reload. so, we append a current datetime query string to the request
-        # so the browser recognizes each load as a new request and doesn't load the
-        # cached version.
-        no_cache = str(datetime.today())
-        return format_html(
-            u'<embed style="max-width: 120px;" src="{0}?d={1}"></embed>',
-            obj.document_url,
-            no_cache
         )
 
 

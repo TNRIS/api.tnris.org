4d34ee6bc3903a76bc1f65ed161140b16f09389f
jwhaney <john.haney@twdb.texas.gov>
2020-03-03 12:22:27 -0600

    save point for all export action tools
    issue #76

--- src/data_hub/lore/actions.py
+++ src/data_hub/lore/actions.py
@@ -4,22 +4,22 @@ import datetime, csv
 from .models import (
     Collection,
     Product,
+    Agency,
     County,
     FrameSize,
     PhotoIndex,
     ScannedPhotoIndexLink,
     LineIndex,
     MicroficheIndex,
-    CountyRelate,
-    ChcView
+    CountyRelate
 )
 
 # additional actions in admin console dropdown
 # export the historical collections table from the database to .csv format
-def export_collections(self, request, queryset):
+def export_collection(self, request, queryset):
     date = datetime.datetime.today().strftime('%Y%m%d')
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="%s_historical_collections.csv"' % date
+    response['Content-Disposition'] = 'attachment; filename="%s_historical_collection.csv"' % date
     writer = csv.writer(response)
     # write header row
     writer.writerow([
@@ -27,70 +27,44 @@ def export_collections(self, request, queryset):
         'collection',
         'from_date',
         'to_date',
+        'agency_id',
+        'photo_index_only',
         'public',
+        'fully_scanned',
         'remarks',
         'created',
         'last_modified',
-        'agency_id',
-        'frames_service_url',
         'index_service_url',
-        'ls4_link',
+        'frames_service_url',
         'mosaic_service_url',
-        'number_of_boxes',
+        'ls4_link',
         'qr_code_url',
-        'fully_scanned',
-        'photo_index_only'
+        'number_of_boxes'
     ])
 
     collections = queryset.values_list()
 
-    # collections = queryset.values_list([
-    #     'id',
-    #     'collection',
-    #     'from_date',
-    #     'to_date',
-    #     'public',
-    #     'remarks',
-    #     'created',
-    #     'last_modified',
-    #     'agency_id',
-    #     'frames_service_url',
-    #     'index_service_url',
-    #     'ls4_link',
-    #     'mosaic_service_url',
-    #     'number_of_boxes',
-    #     'qr_code_url',
-    #     'fully_scanned',
-    #     'photo_index_only'
-    # ])
-
     for c in collections:
-        writer.writerow([
-            c.id,
-            c.collection,
-            c.from_date,
-            c.to_date,
-            c.public,
-            c.remarks,
-            c.created,
-            c.last_modified,
-            c.agency_id,
-            c.frames_service_url,
-            c.
-        ])
+        for a in Agency.objects.filter(collection=c):
+            writer.writerow([
+                c[0],c[1],c[2],c[3],c[4],c[5],c[6],c[7],c[8],c[9],c[10],c[11],c[12],c[13],c[14],c[15],c[16]
+            ])
     return response
-export_collections.short_description = "Collections table to CSV"
+export_collection.short_description = "Collections table to CSV"
 
 
 # export the products table from the database to .csv format
-def export_products(self, request, queryset):
+# frame sizes are based on frame size id's and relationship to frame size table
+# tied to collection records; action available in CollectionAdmin class
+def export_product(self, request, queryset):
     date = datetime.datetime.today().strftime('%Y%m%d')
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="%s_historical_products.csv"' % date
+    response['Content-Disposition'] = 'attachment; filename="%s_historical_product.csv"' % date
     writer = csv.writer(response)
     # writer header row
     writer.writerow([
         'collection_id',
+        'product_id',
         'coverage',
         'number_of_frames',
         'medium',
@@ -99,7 +73,7 @@ def export_products(self, request, queryset):
         'remarks',
         'created',
         'last_modified',
-        'frame_size_id',
+        'frame_size',
         'clean_status'
     ])
 
@@ -107,18 +81,259 @@ def export_products(self, request, queryset):
 
     for c in collections:
         for p in Product.objects.filter(collection=c):
+            for size in FrameSize.objects.filter(product=p):
+                writer.writerow([
+                    p.collection_id,
+                    p.id,
+                    p.coverage,
+                    p.number_of_frames,
+                    p.medium,
+                    p.print_type,
+                    p.physical_location,
+                    p.remarks,
+                    p.created,
+                    p.last_modified,
+                    size.frame_size,
+                    p.clean_status
+                ])
+    return response
+export_product.short_description = "Products table to CSV"
+
+
+# export the photo_index table from the database to .csv format
+# tied to collection records; action available in CollectionAdmin class
+def export_photo_index(self, request, queryset):
+    date = datetime.datetime.today().strftime('%Y%m%d')
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="%s_historical_photo_index.csv"' % date
+    writer = csv.writer(response)
+    # writer header row
+    writer.writerow([
+        'collection_id',
+        'photo_index_id',
+        'number_of_frames',
+        'scanned',
+        'scanned_location',
+        'physical_location',
+        'remarks',
+        'created',
+        'last_modified'
+    ])
+
+    collections = queryset.values_list()
+
+    for c in collections:
+        for photo in PhotoIndex.objects.filter(collection=c):
             writer.writerow([
-                p.collection_id,
-                p.coverage,
-                p.number_of_frames,
-                p.medium,
-                p.print_type,
-                p.physical_location,
-                p.remarks,
-                p.created,
-                p.last_modified,
-                p.frame_size_id,
-                p.clean_status
+                photo.collection_id,
+                photo.id,
+                photo.number_of_frames,
+                photo.scanned,
+                photo.scanned_location,
+                photo.physical_location,
+                photo.remarks,
+                photo.created,
+                photo.last_modified
             ])
     return response
-export_products.short_description = "Products table to CSV"
+export_photo_index.short_description = "Photo Index table to CSV"
+
+
+# export the scanned photo index link table from the database to .csv format
+# tied to collection records; action available in CollectionAdmin class
+def export_scanned_photo_index_link(self, request, queryset):
+    date = datetime.datetime.today().strftime('%Y%m%d')
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="%s_historical_scanned_photo_index_link.csv"' % date
+    writer = csv.writer(response)
+    # writer header row
+    writer.writerow([
+        'collection_id',
+        'link_id',
+        'year',
+        'size',
+        'sheet',
+        'link',
+        'created',
+        'last_modified'
+    ])
+
+    collections = queryset.values_list()
+
+    for c in collections:
+        for link in ScannedPhotoIndexLink.objects.filter(collection=c):
+            writer.writerow([
+                link.collection_id,
+                link.id,
+                link.year,
+                link.size,
+                link.sheet,
+                link.link,
+                link.created,
+                link.last_modified
+            ])
+    return response
+export_scanned_photo_index_link.short_description = "Scanned Photo Index Link table to CSV"
+
+
+# export the county table information using the county relate table
+# tied to collection records; action available in CollectionAdmin class
+def export_county(self, request, queryset):
+    date = datetime.datetime.today().strftime('%Y%m%d')
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="%s_historical_county.csv"' % date
+    writer = csv.writer(response)
+    # writer header row
+    writer.writerow([
+        'collection_id',
+        'county_id',
+        'name',
+        'fips',
+        'created',
+        'last_modified'
+    ])
+
+    collections = queryset.values_list()
+
+    for c in collections:
+        for r in CountyRelate.objects.filter(collection=c):
+            writer.writerow([
+                r.collection_id,
+                r.county.id,
+                r.county.name,
+                r.county.fips,
+                r.created,
+                r.last_modified
+            ])
+    return response
+export_county.short_description = "Counties table to CSV"
+
+
+# export the line_index table information
+# tied to collection records; action available in CollectionAdmin class
+def export_line_index(self, request, queryset):
+    date = datetime.datetime.today().strftime('%Y%m%d')
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="%s_historical_line_index.csv"' % date
+    writer = csv.writer(response)
+    # writer header row
+    writer.writerow([
+        'collection_id',
+        'line_index_id',
+        'remarks',
+        'created',
+        'last_modified'
+    ])
+
+    collections = queryset.values_list()
+
+    for c in collections:
+        for i in LineIndex.objects.filter(collection=c):
+            writer.writerow([
+                i.collection_id,
+                i.id,
+                i.remarks,
+                i.created,
+                i.last_modified
+            ])
+    return response
+export_line_index.short_description = "Line Index table to CSV"
+
+
+# export the agency domain table from the database to .csv format
+# action used in AgencyAdmin
+def export_agency_domain(self, request, queryset):
+    date = datetime.datetime.today().strftime('%Y%m%d')
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="%s_historical_acquiring_agency_domain.csv"' % date
+    writer = csv.writer(response)
+    # write header row
+    writer.writerow([
+        'id',
+        'name',
+        'abbreviation',
+        'sample_image_url',
+        'media_type',
+        'general_scale',
+        'about',
+        'created',
+        'last_modified'
+    ])
+
+    agencies = queryset.values_list(
+        'id',
+        'name',
+        'abbreviation',
+        'sample_image_url',
+        'media_type',
+        'general_scale',
+        'about',
+        'created',
+        'last_modified'
+    )
+
+    for a in agencies:
+        writer.writerow(a)
+
+    return response
+export_agency_domain.short_description = "Acquiring Agency domain table to CSV"
+
+
+# export the frame_size domain table from the database to .csv format
+# action used in FrameSizeAdmin
+def export_frame_size_domain(self, request, queryset):
+    date = datetime.datetime.today().strftime('%Y%m%d')
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="%s_historical_frame_size_domain.csv"' % date
+    writer = csv.writer(response)
+    # write header row
+    writer.writerow([
+        'id',
+        'frame_size',
+        'created',
+        'last_modified'
+    ])
+
+    frame_sizes = queryset.values_list(
+        'id',
+        'frame_size',
+        'created',
+        'last_modified'
+    )
+
+    for f in frame_sizes:
+        writer.writerow(f)
+
+    return response
+export_frame_size_domain.short_description = "Frame Size domain table to CSV"
+
+
+# export the county domain table from the database to .csv format
+# action used in CountyAdmin
+def export_county_domain(self, request, queryset):
+    date = datetime.datetime.today().strftime('%Y%m%d')
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="%s_historical_county_domain.csv"' % date
+    writer = csv.writer(response)
+    # write header row
+    writer.writerow([
+        'id',
+        'fips',
+        'name',
+        'created',
+        'last_modified'
+    ])
+
+    counties = queryset.values_list(
+        'id',
+        'fips',
+        'name',
+        'created',
+        'last_modified'
+    )
+
+    for c in counties:
+        writer.writerow(c)
+
+    return response
+export_county_domain.short_description = "County domain table to CSV"

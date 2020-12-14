65853fc001878ba2d494f8fc28edb46156b27d24
Marlboro Blend No. 27 <adam.breznicky@twdb.texas.gov>
2020-12-14 15:05:37 -0600

    uploaded filename character eval

--- src/data_hub/tnris_org/forms.py
+++ src/data_hub/tnris_org/forms.py
@@ -13,7 +13,7 @@ from .models import (
     TnrisInstructorType,
     TnrisInstructorRelate
 )
-import os
+import os, re
 import boto3, uuid
 
 
@@ -197,6 +197,11 @@ class DocumentForm(forms.ModelForm):
                 # validation to prevent image formats from being uploaded
                 if ext.lower() in invalid_extensions:
                     raise ValidationError(u"Unsupported file extension. All images should be uploaded to 'Tnris Images', only document type files should be uploaded here.")
+                # validation to prevent bad URL characters in filename
+                regex = re.compile('[@!#$%^&*()<>?/\|}{~:]')
+                if regex.search(str(files[f])) is not None:
+                    raise ValidationError(u"Bad character(s) in filename. Filename characters must be URL friendly.")
+                
                 # validation to check if document file name already exists in database
                 name_set = TnrisDocument.objects.filter(document_name=str(files[f]))
                 if len(name_set) > 0:

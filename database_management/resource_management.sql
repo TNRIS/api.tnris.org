99b3678edc1d9e9500dd74f649b868881db6d2b4
Chris Repka <chris.repka@twdb.texas.gov>
2021-03-04 10:09:04 -0600

    sql and models changes
    * Add area_type_name to resource endpoint

--- database_management/resource_management.sql
+++ database_management/resource_management.sql
@@ -11,20 +11,22 @@ SELECT resource.resource_id,
   resource.collection_id,
   resource_type.resource_type_name,
   resource_type.resource_type_abbreviation,
-  area_type.area_type
+  area_type.area_type,
+  area_type.area_type_name
 
 FROM resource
 
 LEFT JOIN resource_type ON resource_type.resource_type_id=resource.resource_type_id
 LEFT JOIN area_type ON area_type.area_type_id=resource.area_type_id
 
-GROUP BY resource.resource_id,
-         resource.resource,
-         resource.filesize,
-         resource.area_type_id,
-         resource.collection_id,
-         resource_type.resource_type_name,
-         resource_type.resource_type_abbreviation,
-         area_type.area_type
+GROUP BY area_type.area_type_name,
+        resource.area_type_id, --group by area_type_id first? any objections?
+		    resource_type.resource_type_name, --group by area_type_name, idea is like-with-like by default
+        resource.resource_id, --
+        resource.resource,
+        resource.filesize,
+        resource.collection_id,
+        resource_type.resource_type_abbreviation,
+        area_type.area_type
 
  WITH DATA;

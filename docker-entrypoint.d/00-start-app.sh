861817ca644933fd8a67d82e447a48de6586bef6
Marlboro Blend No. 27 <adam.breznicky@twdb.texas.gov>
2021-07-29 13:01:56 -0500

    secrets to new service

--- docker-entrypoint.d/00-start-app.sh
+++ docker-entrypoint.d/00-start-app.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
-export SECRET_KEY=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/secret_key --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export DB_NAME=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/db_name --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export DB_USER=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/db_user --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export DB_PASSWORD=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/db_password --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export DB_HOST=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/db_host --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export DB_PORT=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/db_port --with-decryption --region us-east-1 --output text | awk '{print $7}'`
+export SECRET_KEY=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.SECRET_KEY'`
+export DB_NAME=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.DB_NAME'`
+export DB_USER=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.DB_USER'`
+export DB_PASSWORD=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.DB_PASSWORD'`
+export DB_HOST=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.DB_HOST'`
+export DB_PORT=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.DB_PORT'`
 
-export SEO_TOKEN=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/seo_token --with-decryption --region us-east-1 --output text | awk '{print $7}'`
+export SEO_TOKEN=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.SEO_TOKEN'`
 
-export RECAPTCHA_SECRET=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/recaptcha_secret --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export MAIL_DEFAULT_FROM=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/mail_default_from --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export MAIL_DEFAULT_TO=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/mail_default_to --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export AWS_SES_ACCESS_KEY=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/aws_ses_access_key --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export AWS_SES_SECRET_KEY=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/aws_ses_secret_key --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export S3_UPLOAD_BUCKET=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/s3_upload_bucket --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export S3_UPLOAD_SECRET=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/s3_upload_secret --with-decryption --region us-east-1 --output text | awk '{print $7}'`
-export S3_UPLOAD_KEY=`/envs/data_hub/bin/aws ssm get-parameters --name /apps/api_tnris_org/s3_upload_key --with-decryption --region us-east-1 --output text | awk '{print $7}'`
+export RECAPTCHA_SECRET=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.RECAPTCHA_SECRET'`
+export MAIL_DEFAULT_FROM=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.MAIL_DEFAULT_FROM'`
+export MAIL_DEFAULT_TO=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.MAIL_DEFAULT_TO'`
+export AWS_SES_ACCESS_KEY=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.AWS_SES_ACCESS_KEY'`
+export AWS_SES_SECRET_KEY=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.AWS_SES_SECRET_KEY'`
+export S3_UPLOAD_BUCKET=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.S3_UPLOAD_BUCKET'`
+export S3_UPLOAD_SECRET=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.S3_UPLOAD_SECRET'`
+export S3_UPLOAD_KEY=`/envs/data_hub/bin/aws secretsmanager get-secret-value --secret-id api-tnris-org --query SecretString --output text | jq -cr '.S3_UPLOAD_KEY'`
 
 export DATAHUB_MASTER_CLOUDFRONT=`/envs/data_hub/bin/aws cloudformation describe-stack-resource --stack-name datahub-master --logical-resource-id MasterCloudfront --query StackResourceDetail.PhysicalResourceId --output text`
+
+echo "environment variables set!!!"
 
 eval /src/data_hub/manage.py collectstatic --noinput "$@"
 eval /usr/bin/supervisord "$@"

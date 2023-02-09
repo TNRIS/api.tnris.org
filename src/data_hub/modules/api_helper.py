5c0a17adac8fab1135d9d10ee9a52c124162aecc
Levi Harman <levi.harman@twdb.texas.gov>
2023-02-09 15:54:51 -0600

    Add otp authentication

--- src/data_hub/modules/api_helper.py
+++ src/data_hub/modules/api_helper.py
@@ -1,4 +1,4 @@
-import boto3, json, os
+import boto3, json, os, hashlib, time
 from botocore.exceptions import ClientError
 from django.core.mail import EmailMessage
 
@@ -55,3 +55,41 @@ def send_email(
     email = EmailMessage(subject, body, send_from, [send_to], reply_to=[reply_to])
     email.send(fail_silently=False)
     return
+
+def auth_order(auth_details, order_details):
+    """Compare hashes of access code and one time passcode
+
+    Args:
+        auth_details (python object): Body of request sent in from datahub containing access code and otp
+        order_details (python object): Order details from order_details_type
+
+    Returns:
+        boolean: Whether access is denied or allowed.
+    """
+    try:
+        #Gather access token and One time passcode
+        access_token = auth_details["accessCode"]
+        otp = auth_details["passCode"]
+        
+        # Secret salt to stop rainbow tables
+        salt = order_details["access_salt"]
+        
+        # Secret pepper to stop rainbow tables even if salt is known.
+        pepper = get_secret('datahub_order_keys')['access_pepper']
+        
+        # Hash auth_details
+        ac_hash = hashlib.sha256(bytes(access_token + salt + pepper, 'utf8')).hexdigest()
+        otp_hash = hashlib.sha256(bytes(otp + salt + pepper, 'utf8')).hexdigest()
+        
+        # How old in seconds is our otp?
+        otp_age_seconds = time.time() - order_details["otp_age"]
+        
+        # Compare with our stored hash.
+        ACCESS_CODE_VALID = ac_hash == order_details["access_code"]
+        
+        # Compare with stored hash and ensure it's not more than 15 minutes old
+        OTP_VALID = otp_hash == order_details["otp"] and otp_age_seconds < 1800
+    except Exception as e:
+        return False
+    
+    return ACCESS_CODE_VALID and OTP_VALID

f4e70d18a710c66c9765c087fedbe034f6285c92
L-Har <111371248+L-Har@users.noreply.github.com>
2023-08-02 14:02:33 -0500

    return a json representation

--- src/data_hub/tnris_org/viewsets.py
+++ src/data_hub/tnris_org/viewsets.py
@@ -205,7 +205,7 @@ class TnrisMetricsViewSet(viewsets.ViewSet):
     def create(self, request, format=None):
         try:
             s3_client = boto3.client('s3')
-            html = s3_client.get_object(Bucket='tnris-analytics', Key='analytics.html')
+            html = s3_client.get_object(Bucket='tnris-analytics', Key='analytics.json')
             resp = html['Body'].read()
 
             return Response(

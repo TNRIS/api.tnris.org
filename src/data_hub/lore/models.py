9fb6bb9a532e722d84d02a5e0300be155d771f3e
L-Har <111371248+L-Har@users.noreply.github.com>
2023-11-29 13:08:20 -0600

    add dl_link to historical

--- src/data_hub/lore/models.py
+++ src/data_hub/lore/models.py
@@ -230,6 +230,7 @@ class Collection(models.Model):
     frames_service_url = models.URLField('Frames Service URL', max_length=256, null=True, blank=True)
     mosaic_service_url = models.URLField('Mosaic Service URL', max_length=256, null=True, blank=True)
     ls4_link = models.CharField(max_length=200, null=True, blank=True)
+    dl_link = models.CharField('Download Link', max_length=250, null=True, blank=True)
     qr_code_url = models.URLField('QR Code URL', max_length=256, null=True, blank=True)
     number_of_boxes = models.PositiveIntegerField('Number of Boxes', null=True, blank=True)
     thumbnail_image = models.TextField(

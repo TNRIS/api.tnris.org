ffbc141696b02e77aa9a5713c994c9889d76c0d4
Rob Dennett <rob.dennett@twdb.texas.gov>
2024-05-03 08:06:06 -0500

    Removes unused code

--- src/data_hub/analytics/urls.py
+++ src/data_hub/analytics/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('get_monthly_stats/', views.get_monthly_stats)
-    # path('hello/', views.hello_world),
-    # path('get_monthly_stats_old/', views.get_monthly_stats_old)
 ]

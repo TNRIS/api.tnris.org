028ee1f3fbbfcfaffa6ea36af000390ca611ebff
L-Har <111371248+L-Har@users.noreply.github.com>
2024-12-13 16:06:25 -0600

    Testing fiserv endpoint modifications (#432)

--- src/data_hub/contact/tests.py
+++ src/data_hub/contact/tests.py
@@ -5,14 +5,20 @@ import json
 from django.core import mail
 from django.test import TestCase, AsyncRequestFactory
 from contact.viewsets import (
+    GenOtpViewSet,
     SubmitFormViewSet,
     OrderSubmitViewSet,
     OrderFormViewSet,
+    InitiateRetentionCleanupViewSet,
+    OrderCleanupViewSet
 )
 from contact.new_contacts import (
     GenOtpViewSetSuper,
-    OrderSubmitViewSetSuper,
     OrderFormViewSetSuper,
+    OrderSubmitViewSetSuper,
+    OrderStatusViewSetSuper,
+    InitiateRetentionCleanupViewSetSuper,
+    OrderCleanupViewSetSuper
 )
 from .models import OrderType
 from rest_framework.request import Request
@@ -39,7 +45,95 @@ def create_super_request(data, query_params=""):
     return django_request
 
 
-class OrderFormTestCase(TestCase):
+class GeneralTest(TestCase):
+    """Inheritable with some common setup."""
+
+    fixtures = ["email_templates.json"]
+    uuid = ""
+    order_details_new = {
+        "Name": "FIRSTNAME LASTNAME",
+        "Email": "TxGIO-DevOps@twdb.texas.gov",
+        "Phone": "1112223333",
+        "Address": "State Parking Garage E",
+        "City": "Austin",
+        "State": "Texas",
+        "Zipcode": "78660",
+        "Organization": "TEST",
+        "Industry": "TESTING",
+        "Fedex": "",
+        "Notes": "Test Note",
+        "Delivery": "Test Delivery",
+        "HardDrive": "Test Hard Drive",
+        "Payment": "CC",
+        "Order": "Test item 1\nTest Item 2",
+        "form_id": "data-tnris-org-order",
+    }
+
+    def setUp(self):
+        order = OrderFormViewSetSuper.create_order_object(
+            self,
+            email="TxGIO-DevOps@twdb.texas.gov",
+            order_details=self.order_details_new,
+            test_otp="12345",
+        )
+        order.order_approved = True
+        order.approved_charge = 7.0
+        order.save()
+        self.uuid = str(order.id)
+        # # Make sure email is sent.
+        self.assertEqual(
+            len(mail.outbox), 1, "No email has been sent. in GeneralTest.setup()"
+        )
+
+class OrderCleanupTestCase(GeneralTest): # Tested done pt1.
+    """Order cleanup test case."""
+
+    def test_order_cleanup(self):
+        order_cleanup = OrderCleanupViewSetSuper()
+        request = create_super_request(
+            {"access_code": os.environ.get("CCP_ACCESS_CODE")}
+        )
+        response = order_cleanup.create(request)
+
+        print("Break")
+
+class InitiateRetentionCleanupTestCase(GeneralTest):  # Tested done pt1.
+    """Initiate retention cleanup test case."""
+
+    def test_initiate_retentions_cleanup(self):
+        initiate_cleanup = InitiateRetentionCleanupViewSetSuper()
+        request = create_super_request(
+            {"approve_run": "none", "access_code": os.environ.get("CCP_ACCESS_CODE")}
+        )
+        response = initiate_cleanup.create(request)
+        self.assertEquals(response.status_code, 200, "Order status successful.")
+
+
+class OrderStatusTestCase(GeneralTest):  # Tested done pt1.
+    """Query Fiserv for order status"""
+
+    def test_order_status(self):
+        """Test getting order status"""
+        order_status_super = OrderStatusViewSetSuper()
+        request = create_super_request(
+            {
+                "recaptcha": "none",
+                "accessCode": "TxGIO-DevOps@twdb.texas.gov",
+                "form_id": "order-map",
+                "EMAIL": os.environ.get(
+                    "MAIL_DEFAULT_TO"
+                ),  # Caps to test that route makes keys lowercase
+                "passCode": "12345",  # Note: this is not a real passcode.
+            },
+            f"?uuid={self.uuid}",
+        )
+        response = order_status_super.create(request)
+
+        # Make sure we get a 200 created response.
+        self.assertEquals(response.status_code, 200, "Order status unsuccessful.")
+
+
+class OrderFormTestCase(TestCase):  # Tested done pt1.
     """Test Order Form Submission"""
 
     fixtures = ["email_templates.json"]
@@ -62,14 +156,10 @@ class OrderFormTestCase(TestCase):
             {
                 "recaptcha": "none",
                 "form_id": "data-tnris-org-order",
-                "order_details": {
-                    "item": 50,
-                    "PAYMENT": 5.42
-                },
+                "order_details": {"item": 50, "PAYMENT": 5.42},
                 "EMAIL": os.environ.get(
                     "MAIL_DEFAULT_TO"
                 ),  # Caps to test that route makes keys lowercase
-                
             }
         )
         response = order_form_super.create(request)
@@ -103,13 +193,10 @@ class OrderFormTestCase(TestCase):
             {
                 "recaptcha": "none",
                 "form_id": "order-map",
-                "order_details": {
-                    "item": 50, 
-                    "PAYMENT": 5.43
-                },
+                "order_details": {"item": 50, "PAYMENT": 5.43},
                 "EMAIL": os.environ.get(
                     "MAIL_DEFAULT_TO"
-                ),# Caps to test that route makes keys lowercase
+                ),  # Caps to test that route makes keys lowercase
             }
         )
         response = order_form_super.create(request)
@@ -122,7 +209,8 @@ class OrderFormTestCase(TestCase):
         # Expected two emails to be sent.
         self.assertEqual(len(mail.outbox), 2, "No email has been sent.")
 
-class GoneTestCase(TestCase):
+
+class GoneTestCase(TestCase):  # Tested done pt1.
     """Test that old order urls are gone."""
 
     def test_gone(self):
@@ -135,55 +223,51 @@ class GoneTestCase(TestCase):
             response.status_code, 410, "Submit route not sending 410 GONE signal."
         )
 
-class GenOtpTestCase(TestCase):
-    """Test OTP."""
 
-    fixtures = ["email_templates.json"]
+class GenOtpTestCase(GeneralTest):  # Tested done pt1.
+    """Test OTP."""
 
     def test_gen_otp_form_blocking(self):
         """Test the OTP blocks failed captchas."""
         request = requests.Request(method="POST", data={"recaptcha": "none"})
 
         # build a basic request to test with
-        request = create_super_request({"recaptcha": "none"}, "?uuid=''")
+        request = create_super_request({"recaptcha": "none"}, f"?uuid={self.uuid}")
+
+        genotp_form = GenOtpViewSet()
+        response = genotp_form.create(request)
+
+        self.assertEquals(
+            response.status_code,
+            403,
+            "Generate OTP route not sending 403 Forbidden signal.",
+        )
+
+    def test_gen_otp(self):
+        """Test the OTP generation process."""
+        request = requests.Request(method="POST", data={"recaptcha": "none"})
+
+        # build a basic request to test with
+        request = create_super_request({"recaptcha": "none"}, f"?uuid={self.uuid}")
 
         genotp_form = GenOtpViewSetSuper()
         response = genotp_form.create(request)
-
-class OrderSubmitTestCase(TestCase):
-    """Test Order Submit"""
-
-    fixtures = ["email_templates.json"]
-    uuid = ""
-    order_details_new = {
-        "Name": "FIRSTNAME LASTNAME",
-        "Email": "TxGIO-DevOps@twdb.texas.gov",
-        "Phone": "1112223333",
-        "Address": "State Parking Garage E",
-        "City": "Austin",
-        "State": "Texas",
-        "Zipcode": "78660",
-        "Organization": "TEST",
-        "Industry": "TESTING",
-        "Fedex": "",
-        "Notes": "Test Note",
-        "Delivery": "Test Delivery",
-        "HardDrive": "Test Hard Drive",
-        "Payment": "CC",
-        "Order": "Test item 1\nTest Item 2",
-        "form_id": "data-tnris-org-order"
-    }
-    def setUp(self):
-        order = OrderFormViewSetSuper.create_order_object(
-            self,
-            email="TxGIO-DevOps@twdb.texas.gov",
-            order_details=self.order_details_new,
-            test_otp="12345",
+        self.assertEquals(
+            response.status_code,
+            200,
+            "Test the OTP generation failed.",
         )
-        order.order_approved = True
-        order.approved_charge = 7.0
-        order.save()
-        self.uuid = str(order.id)
+
+        self.assertEquals(
+            response.data["status"],
+            "success",
+            "Test the OTP generation failed.",
+        )
+        print("Breakpoint test")
+
+
+class OrderSubmitTestCase(GeneralTest):  # Tested done pt1.
+    """Test Order Submit"""
 
     def test_gen_order_submit_blocking(self):
         """Test the order submit blocks failed captchas."""
@@ -197,26 +281,26 @@ class OrderSubmitTestCase(TestCase):
         )
 
     def test_order_submit(self):
-        """Test Order Submit create function"""
+        """Test creating HPP Link"""
         submit_form_super = OrderSubmitViewSetSuper()
         request = create_super_request(
             {
                 "recaptcha": "none",
                 "form_id": "data-tnris-org-order",
                 "accessCode": "TxGIO-DevOps@twdb.texas.gov",
-                "passCode": "12345", #Note: this is not a real passcode. 
+                "passCode": "12345",  # Note: this is not a real passcode.
             },
             f"?uuid={self.uuid}",
         )
         response = submit_form_super.create(request)
 
         # # Make sure we get a 201 created response.
-        # self.assertEquals(response.status_code, 201, "Form submission was not successful.")
+        self.assertEquals(
+            response.status_code, 200, "Form submission and HPP was not created."
+        )
 
-        # # Make sure email is sent.
-        # self.assertEqual(len(mail.outbox), 1, "No email has been sent.")
 
-class FiservTestCase(TestCase):
+class FiservTestCase(TestCase):  # Tested done pt1.
     """Test Fiserv endpoints"""
 
     def test_make_hmac(self):
@@ -226,7 +310,7 @@ class FiservTestCase(TestCase):
             "POST",
             json.dumps(payload_valid_test),
             os.environ.get("FISERV_DEV_ACCOUNT_ID"),
-            os.environ.get('FISERV_DEV_AUTH_CODE')
+            os.environ.get("FISERV_DEV_AUTH_CODE"),
         )
         self.assertIsNotNone(hmac)
         basic = api_helper.generate_basic_auth()
@@ -234,11 +318,17 @@ class FiservTestCase(TestCase):
             FISERV_URL + "GetRequestID",
             json=payload_valid_test,
             headers={
-                "accountid": os.environ.get("FISERV_DEV_ACCOUNT_ID"), # good
-                "merchantid": os.environ.get("FISERV_MERCHANT_ID"), # good
+                "accountid": os.environ.get("FISERV_DEV_ACCOUNT_ID"),  # good
+                "merchantid": os.environ.get("FISERV_MERCHANT_ID"),  # good
                 "signature": f"Hmac {hmac.decode()}",
-                "Authorization": f"Basic {basic.decode()}"
-            })
+                "Authorization": f"Basic {basic.decode()}",
+            },
+        )
 
         self.assertIs(response.status_code, 200, "response status_code is not 200")
-        print("Error: " + str(response.status_code) + " " + json.loads(response.text)["message"])
+        print(
+            "Error: "
+            + str(response.status_code)
+            + " "
+            + json.loads(response.text)["message"]
+        )

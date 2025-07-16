8922b4ff62f6239090b754d2907389adacf16076
L-Har <111371248+L-Har@users.noreply.github.com>
2025-07-16 13:19:58 -0500

    Create a new api helper, backup the old one, and a fiserv helper.

---
+++ src/data_hub/contact/fiserv_routines/fiserv_helper.py
@@ -0,0 +1,253 @@
+import datetime, uuid, base64, hmac, os, hashlib
+
+api_url = str(os.environ.get('API_URL'))
+
+def computeSignature(requestURI: str, requestMethod: str, payLoad: str, accountId: str, key: str):
+    """
+    Compute a HMAC signature for fiserv
+    """
+    requestTimeStamp: str = str(int(datetime.datetime.now().timestamp()))
+    nonce: str = str(uuid.uuid4())
+    requestContentBase64String: str = payLoad
+    signatureRawData: str = accountId + requestMethod + requestURI + requestTimeStamp +  nonce + requestContentBase64String
+    secretKeyByteArray = base64.b64decode(key)
+    signature = signatureRawData.encode("utf8")
+    signatureBytes = hmac.new(secretKeyByteArray, signature, hashlib.sha256)
+    requestSignatureBase64String = base64.b64encode(signatureBytes.digest())
+    HmacRaw = f"{accountId}:{requestSignatureBase64String.decode()}:{nonce}:{requestTimeStamp}"
+    HmacValue = base64.b64encode(HmacRaw.encode("utf8"))
+    return HmacValue
+
+def generate_fiserv_hmac(
+    url: str,
+    method: str,
+    payLoad: str,
+    accountId: str,
+    key: str
+):
+    return computeSignature(url, method, payLoad, accountId, key)
+
+def generate_basic_auth():
+    """
+    Generate basic authentication token for fiserv
+    """
+    basic = f"{os.environ.get('FISERV_API_BASIC_AUTH_USERNAME')}:{os.environ.get('FISERV_API_BASIC_AUTH_PWD')}"
+    basic = base64.b64encode(basic.encode("utf8"))
+    return basic
+
+def generate_fiserv_post_body(
+        payment_method: str,
+        order_details_id: str,
+        template_id: int,
+        total: float,
+        transactionfee: float,
+        order_details: object,
+        charge: bool = False
+    ):
+    """
+    Return an object to be used in fdms body.
+    Args:
+        payment_method (str): Payment type of this transaction. Either ACH or CC.
+        order_details_id (str): unique order id.
+        template_id (str): Template id.
+        total (float): Total of purchase without fee.
+        transactionfee (float): DIR fee.
+        order_details (object): Order details object.
+        charge (bool): Whether we are using the charge api. Defaults to False
+    """
+
+    f_tran_amount  = "{:.2f}".format(round(total + transactionfee, 2))
+    f_total_amount = "{:.2f}".format(total)
+    f_tran_fee = "{:.2f}".format(transactionfee)
+
+    clx_struct = generate_clx_stream(f_total_amount, f_tran_fee, order_details_id, payment_method)
+
+    # In the HPP v3 api as of June 5, 2025 clxstream must be enclosed in an array.
+    if not charge:
+        clx_struct = [clx_struct]
+
+    post_body = {
+        "accountid": os.environ.get("FISERV_DEV_ACCOUNT_ID"),  # required
+        "companycode": os.environ.get("FISERV_COMPANY_CODE"),  # required
+        "currencycode": "USD",  # required
+        "customerid": os.environ.get("FISERV_CUSTOMER_ID"),  # required
+        "userid": os.environ.get("FISERV_USER_ID"),  # required
+        "redirecturl": "https://" + api_url + "/api/v2/contact/order/redirect?status=success",
+        "cancelredirecturl": "https://" + api_url +"/api/v2/contact/order/redirect?status=cancel",  # Optional but we can use it.
+        "reference": "UPI",  # Required
+        "templateid": template_id,  # required
+        "transactiontype": "S",  # required: S means for a sale.
+        "transactionamount": f_tran_amount,  # required
+        "paymentmode": payment_method,  # As needed.
+        "sendemailreceipts": "Y",
+        "cof": "C",  # Optional, means Card on file, and C means customer.
+        "cofscheduled": "N",  # Optional, N means no don't schedule card to be filed.
+        "ecomind": "E",  # Optional, E means ECommerce, this is a note on the origin of transaction
+        "orderid": "580WD" + order_details_id,  # Local order ID; we use it as a reference to get transaction info.
+        # "purchaseorder": "", Optional,
+        "type": "C",  # Optional, but C means customer.
+        "savepaymentmethod": "N",  # Optional
+        "saveatcustomer": "N",  # Optional
+        "displaycardssavedatcustomer": "N",  # Optional
+        "customer": {  # Optional customer data
+            "customername": order_details["name"],
+            "addressline1": order_details["address"],
+            "addressline2": "",
+            "city": order_details["city"],
+            "state": order_details["state"],
+            "zipcode": order_details["zipcode"],
+            "country": "",  # Add state to order_details
+            "phone": order_details["phone"],
+            "email": order_details["email"],
+        },
+        "payments": [  # required
+            {
+                "mode": payment_method,
+                "merchantid": os.environ.get("FISERV_MERCHANT_ID"),
+            }
+        ],
+        "level3": generate_level_three(f_total_amount, f_tran_fee),
+        "clxstream": clx_struct
+    }
+
+    if payment_method == "ACH":
+        post_body["payments"]["achseccode"] = "WEB"
+        post_body["payments"]["achsecdescriptor"] = "PURCHASE"
+        post_body["payments"]["feeachseccode"] = "WEB"
+        post_body["payments"]["feeachsecdescriptor"] = "PURCHASE"
+
+    return post_body
+
+def generate_clx_stream(f_total_amount, f_tran_fee, order_details_id, payment_method):
+    """
+    """
+    return {
+            "transaction": {
+                "agency": "580",
+                "batchid": "",
+                "company": "Texas Water Development Board",
+                "customerid": os.environ.get("FISERV_CUSTOMER_ID"),
+                "department": "Texas Geographic Information Office",
+                "description": "TxGIO DataHub order",
+                "fee": f_tran_fee,
+                "localreferenceid": "580WD" + order_details_id,
+                "merchantid": os.environ.get("FISERV_MERCHANT_ID"),
+                "paymenttype": payment_method,
+                "quantity": "1",
+                "reportlines": "3",  # This should be how many report line details attributes.
+                "sku": "DHUB",  # Should be correct.
+                "type": "ecommerce",
+                "unitprice": f_total_amount,
+                "vendorid": os.environ.get("FISERV_SERVICE_CODE"),
+                "reportlinedetails": [{
+                    "id": "USAS1",
+                    "attributes": [
+                        {
+                            "name": "USAS1COBJ",
+                            "value": "3719",
+                            "type": "String",
+                        },
+                        {
+                            "name": "USAS1PCA",
+                            "value": "19001",
+                            "type": "String",
+                        },
+                        {
+                            "name": "USAS1TCODE",
+                            "value": "195",
+                            "type": "String",
+                        },
+                        {
+                            "name": "USAS1AMOUNT",
+                            "value": f_total_amount,
+                            "type": "DOLLAR",
+                        },
+                    ],
+                },
+                {
+                    "id": "USAS2",
+                    "attributes": [
+                        {
+                            "name": "USAS2COBJ",
+                            "value": "3879",
+                            "type": "String",
+                        },
+                        {
+                            "name": "USAS2PCA",
+                            "value": "07768",
+                            "type": "String",
+                        },
+                        {
+                            "name": "USAS2TCODE",
+                            "value": "179",
+                            "type": "String",
+                        },
+                        {
+                            "name": "USAS2AMOUNT",
+                            "value": f_tran_fee,
+                            "type": "DOLLAR",
+                        },
+                    ],
+                },
+                {
+                    "id": "USAS3",
+                    "attributes": [
+                        {
+                            "name": "USAS3COBJ",
+                            "value": "7219",
+                            "type": "String",
+                        },
+                        {
+                            "name": "USAS3TCODE",
+                            "value": "265",
+                            "type": "String",
+                        },
+                        {
+                            "name": "USAS3PCA",
+                            "value": "07768",
+                            "type": "String",
+                        },
+                        {
+                            "name": "USAS3AMOUNT",
+                            "value": f_tran_fee,
+                            "type": "DOLLAR",
+                        },
+                    ],
+                }],
+                "additionalinfo": [
+                    {
+                        "key": "servicecode",
+                        "value": os.environ.get("FISERV_SERVICE_CODE"),
+                    }
+                ],
+            }
+        }
+    
+
+def generate_level_three(f_total_amount, f_tran_fee): 
+    return [
+        {
+            "linenumber": "1.000",
+            "productcode": "TXGIO_DATA",
+            "taxrate": "0",
+            "quantity": "1",
+            "itemdescriptor": "TxGIO DataHub order",
+            "unitcost": f_total_amount,
+            "lineitemtotal": f_total_amount,
+            "taxamount": "0",
+            "commoditycode": "",
+            "unitofmeasure": "EA",
+        },
+        {
+            "linenumber": "2.000",
+            "productcode": "TXDIR_FEE",
+            "taxrate": "0",
+            "quantity": "1",
+            "itemdescriptor": "Texas.gov fee**",
+            "unitcost": f_tran_fee,
+            "lineitemtotal": f_tran_fee,
+            "taxamount": "0",
+            "commoditycode": "",
+            "unitofmeasure": "EA",
+        },
+    ]
--- src/data_hub/modules/api_helper.py
+++ src/data_hub/modules/api_helper.py
@@ -1,18 +1,167 @@
-import boto3, json, os, hashlib, time, requests
-from botocore.exceptions import ClientError
-from django.core.mail import send_mail
-from django.core.mail import EmailMessage, EmailMultiAlternatives
-import logging, watchtower
+"""
+Api Helper
+"""
+
+import logging
 import time
+import hashlib
+import os
+import json
+import boto3
+import requests
+import watchtower
+from botocore.exceptions import ClientError
+from django.core.mail import EmailMessage, EmailMultiAlternatives
 from lcd.models import LoggerType
+from rest_framework.permissions import AllowAny
+from cryptography.fernet import Fernet
+
 logger = logging.getLogger("errLog")
 logger.addHandler(watchtower.CloudWatchLogHandler())
 
 SHOULD_LOG = False
 LAST_CHECKED = 0
+FISERV_URL = "https://snappaydirectapi-cert.fiserv.com/api/interop/"
+
+
+class CorsPostPermission(AllowAny):
+    """
+    custom permissions for cors control
+    """
+
+    whitelisted_domains = [
+        "api.tnris.org",
+        "beta.tnris.org",
+        "data.tnris.org",
+        "alphabetadata.tnris.org",
+        "api-test.tnris.org",
+        "betadata.tnris.org",
+        "develop.tnris.org",
+        "lake-gallery.tnris.org",
+        "localhost:8000",
+        "localhost",
+        "staging.tnris.org",
+        "stagingapi.tnris.org",
+        "staginghub.tnris.org",
+        "store.tnris.org",
+        "tnris.org",
+        "www.tnris.org",
+        "dev.txgio.org",
+        "geographic.texas.gov",
+        "dev.geographic.texas.gov",
+        "staging.geographic.texas.gov",
+        "dev.gio.texas.gov",
+        "staging.gio.texas.gov",
+        "data.geographic.texas.gov",
+        "data.gio.texas.gov",
+        "devdata.geographic.texas.gov",
+        "stagingdata.geographic.texas.gov",
+        "cmsdev",
+        "cmsprod",
+    ]
+
+    def has_permission(self, request, view):
+        u = (
+            urlparse(request.META["HTTP_REFERER"]).hostname
+            if "HTTP_REFERER" in request.META.keys()
+            else request.META["HTTP_HOST"]
+        )
+        return u in self.whitelisted_domains
+
+
+class CorsPostPermission(AllowAny):
+    """
+    custom permissions for cors control
+    """
+
+    whitelisted_domains = [
+        "api.tnris.org",
+        "beta.tnris.org",
+        "data.tnris.org",
+        "alphabetadata.tnris.org",
+        "api-test.tnris.org",
+        "betadata.tnris.org",
+        "develop.tnris.org",
+        "lake-gallery.tnris.org",
+        "localhost:8000",
+        "localhost",
+        "staging.tnris.org",
+        "stagingapi.tnris.org",
+        "staginghub.tnris.org",
+        "store.tnris.org",
+        "tnris.org",
+        "www.tnris.org",
+        "dev.txgio.org",
+        "geographic.texas.gov",
+        "dev.geographic.texas.gov",
+        "staging.geographic.texas.gov",
+        "dev.gio.texas.gov",
+        "staging.gio.texas.gov",
+        "data.geographic.texas.gov",
+        "data.gio.texas.gov",
+        "devdata.geographic.texas.gov",
+        "stagingdata.geographic.texas.gov",
+        "cmsdev",
+        "cmsprod",
+    ]
+
+    def has_permission(self, request, view):
+        u = (
+            urlparse(request.META["HTTP_REFERER"]).hostname
+            if "HTTP_REFERER" in request.META.keys()
+            else request.META["HTTP_HOST"]
+        )
+        return u in self.whitelisted_domains
+
+
+
+class CorsPostPermission(AllowAny):
+    """
+    custom permissions for cors control
+    """
+
+    whitelisted_domains = [
+        "api.tnris.org",
+        "beta.tnris.org",
+        "data.tnris.org",
+        "alphabetadata.tnris.org",
+        "api-test.tnris.org",
+        "betadata.tnris.org",
+        "develop.tnris.org",
+        "lake-gallery.tnris.org",
+        "localhost:8000",
+        "localhost",
+        "staging.tnris.org",
+        "stagingapi.tnris.org",
+        "staginghub.tnris.org",
+        "store.tnris.org",
+        "tnris.org",
+        "www.tnris.org",
+        "dev.txgio.org",
+        "geographic.texas.gov",
+        "dev.geographic.texas.gov",
+        "staging.geographic.texas.gov",
+        "dev.gio.texas.gov",
+        "staging.gio.texas.gov",
+        "data.geographic.texas.gov",
+        "data.gio.texas.gov",
+        "devdata.geographic.texas.gov",
+        "stagingdata.geographic.texas.gov",
+        "cmsdev",
+        "cmsprod",
+    ]
+
+    def has_permission(self, request, view):
+        u = (
+            urlparse(request.META["HTTP_REFERER"]).hostname
+            if "HTTP_REFERER" in request.META.keys()
+            else request.META["HTTP_HOST"]
+        )
+        return u in self.whitelisted_domains
+
 
 def get_secret(secret_name):
-    """ Access all of the AWS Secrets via it's Identifier.
+    """Access all of the AWS Secrets via it's Identifier.
 
     Args:
         secret_name (string): Name of the Secret in AWS secrets manager
@@ -27,31 +176,27 @@ def get_secret(secret_name):
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-    
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
     try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
         # For a list of exceptions thrown, see
         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
         raise e
 
     # Decrypts secret using the associated KMS key.
-    return json.loads(get_secret_value_response['SecretString'])
+    return json.loads(get_secret_value_response["SecretString"])
 
-def send_email(
-        subject,
-        body,
-        send_from=os.environ.get("MAIL_DEFAULT_FROM"),
-        send_to=os.environ.get("MAIL_DEFAULT_TO"),
-        reply_to="unknown@tnris.org",
-    ):
-    """ Generic function for sending emails from the API
+def send_html_email(
+    subject,
+    body,
+    send_from=os.environ.get("MAIL_DEFAULT_FROM"),
+    send_to=os.environ.get("MAIL_DEFAULT_TO"),
+    reply_to="unknown@tnris.org",
+    cc_email=""
+):
+    """Generic function for sending emails from the API
 
     Args:
         subject (String): Subject header for the email
@@ -59,10 +204,13 @@ def send_email(
         send_from (String, optional): Email to send from.. Defaults to os.environ.get("MAIL_DEFAULT_FROM").
         send_to (_type_, optional): Email Address to receive.. Defaults to os.environ.get("MAIL_DEFAULT_TO").
         reply_to (str, optional): Email address to reply to.. Defaults to "unknown@tnris.org".
+        cc_email (str, optional): cc email
     """
 
-    mail = EmailMultiAlternatives(subject, body, send_from, [send_to], reply_to=[reply_to])
-    mail.attach_alternative(body, 'text/html')
+    mail = EmailMultiAlternatives(
+        subject, body, send_from, [send_to], [cc_email], reply_to=[reply_to]
+    )
+    mail.attach_alternative(body, "text/html")
 
     return mail.send()
 
@@ -72,8 +220,14 @@ def send_raw_email(
     send_from=os.environ.get("MAIL_DEFAULT_FROM"),
     send_to=os.environ.get("MAIL_DEFAULT_TO"),
     reply_to="unknown@tnris.org",
+    cc_email=""
 ):
-    email = EmailMessage(subject, body, send_from, [send_to], reply_to=[reply_to])
+    """
+    generic function for sending email associated with form submission
+    emails send to supportsystem to create tickets in the ticketing system
+    which are ultimately managed by IS, RDC, and StratMap
+    """
+    email = EmailMessage(subject, body, send_from, [send_to], [cc_email], reply_to=[reply_to])
     email.send(fail_silently=False)
     return
 
@@ -89,82 +243,106 @@ def auth_order(auth_details, order):
     """
     try:
         order_details = order.order_details
-        #Gather access token and One time passcode
+        # Gather access token and One time passcode
         access_token = auth_details["accessCode"]
         otp = auth_details["passCode"]
-        
+
         # Secret salt to stop rainbow tables
         salt = order_details.access_salt
-        
-        # Secret pepper to stop rainbow tables even if salt is known.
+
+        # Secret pepper. 
         pepper = os.environ.get("ACCESS_PEPPER")
-        
+
         # Hash auth_details
-        ac_hash = hashlib.sha256(bytes(access_token + salt + pepper, 'utf8')).hexdigest()
-        otp_hash = hashlib.sha256(bytes(otp + salt + pepper, 'utf8')).hexdigest()
-        
+        ac_hash = hashlib.sha256(
+            bytes(access_token + salt + pepper, "utf8")
+        ).hexdigest()
+        otp_hash = hashlib.sha256(bytes(otp + salt + pepper, "utf8")).hexdigest()
+
         # How old in seconds is our otp?
         otp_age_seconds = time.time() - order_details.otp_age
-        
+
         # Compare with our stored hash.
         ACCESS_CODE_VALID = ac_hash == order_details.access_code
-        
+
         # Compare with stored hash and ensure it's not more than 15 minutes old
         OTP_VALID = otp_hash == order_details.otp and otp_age_seconds < 1800
     except Exception as e:
         return False
-    if(ACCESS_CODE_VALID and OTP_VALID):
-        #Invalidate access code after first success.
+    if ACCESS_CODE_VALID and OTP_VALID:
+        # Invalidate access code after first success.
         order.order_details.otp_age = 0
         order.order_details.save()
         order.save()
     return ACCESS_CODE_VALID and OTP_VALID
 
-def checkCaptcha(IS_DEBUG, captcha):
+def checkCaptcha(captcha):
     """Check a captcha string for success.
 
     Args:
-        IS_DEBUG (boolean): Whether we are running in debug mode.
         captcha (_type_): String sent in request body to check captcha success/failure
 
     Returns:
         _type_: python object with information about status of captcha.
     """
-    logger.info("Checking Captcha.")
-
-    # if in DEBUG mode, assume local development and use localhost recaptcha secret
-    # otherwise, use product account secret environment variable
-    recaptcha_secret = (
-        "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
-        if IS_DEBUG
-        else os.environ.get("RECAPTCHA_SECRET")
-    )
+   
+    recaptcha_secret = os.environ.get("RECAPTCHA_SECRET")
     recaptcha_verify_url = "https://www.google.com/recaptcha/api/siteverify"
     recaptcha_data = {
         "secret": recaptcha_secret,
         "response": captcha,
     }
-    
+
     return requests.post(url=recaptcha_verify_url, data=recaptcha_data)
 
-def buildOrderString(order_obj): 
+def buildOrderString(order_obj):
     order_string = ""
-    order_string += "Name: " + (order_obj["Name"] + "\n" if "Name" in order_obj else "\n")
-    order_string += "Email: " + (order_obj["Email"] + "\n" if "Email" in order_obj else "\n")
-    order_string += "Phone: " + (order_obj["Phone"] + "\n" if "Phone" in order_obj else "\n")
-    order_string += "Address: " + (order_obj["Address"] + "\n" if "Address" in order_obj else "\n")
-    order_string += "Organization: " + (order_obj["Organization"] + "\n" if "Organization" in order_obj else "\n")
-    order_string += "Industry: " + (order_obj["Industry"] + "\n" if "Industry" in order_obj else "\n")
-    order_string += "-------------------------------------------------------------------\n"
-    order_string += "Hard Drive: " + (order_obj["HardDrive"] + "\n" if "HardDrive" in order_obj else "\n")
-    order_string += "Delivery: " + (order_obj["Delivery"] + "\n" if "Delivery" in order_obj else "\n")
-    order_string += "Payment: " + (order_obj["Payment"] + "\n" if "Payment" in order_obj else "\n")
-    order_string += "-------------------------------------------------------------------\n"
-    order_string += "Notes: " + (order_obj["Notes"] + "\n" if "Notes" in order_obj else "\n")
-    order_string += ("Fedex ID: " + order_obj["Fedex"] + "\n" if "Fedex" in order_obj and len(order_obj["Fedex"].strip()) > 0 else "\n")
+    order_string += "Name: " + (
+        order_obj["Name"] + "\n" if "Name" in order_obj else "\n"
+    )
+    order_string += "Email: " + (
+        order_obj["Email"] + "\n" if "Email" in order_obj else "\n"
+    )
+    order_string += "Phone: " + (
+        order_obj["Phone"] + "\n" if "Phone" in order_obj else "\n"
+    )
+    order_string += "Address: " + (
+        order_obj["Address"] + "\n" if "Address" in order_obj else "\n"
+    )
+    order_string += "Organization: " + (
+        order_obj["Organization"] + "\n" if "Organization" in order_obj else "\n"
+    )
+    order_string += "Industry: " + (
+        order_obj["Industry"] + "\n" if "Industry" in order_obj else "\n"
+    )
+    order_string += (
+        "-------------------------------------------------------------------\n"
+    )
+    order_string += "Hard Drive: " + (
+        order_obj["HardDrive"] + "\n" if "HardDrive" in order_obj else "\n"
+    )
+    order_string += "Delivery: " + (
+        order_obj["Delivery"] + "\n" if "Delivery" in order_obj else "\n"
+    )
+    order_string += "Payment: " + (
+        order_obj["Payment"] + "\n" if "Payment" in order_obj else "\n"
+    )
+    order_string += (
+        "-------------------------------------------------------------------\n"
+    )
+    order_string += "Notes: " + (
+        order_obj["Notes"] + "\n" if "Notes" in order_obj else "\n"
+    )
+    order_string += (
+        "Fedex ID: " + order_obj["Fedex"] + "\n"
+        if "Fedex" in order_obj and len(order_obj["Fedex"].strip()) > 0
+        else "\n"
+    )
     order_string += "\n"
-    order_string += "-------------------------------------------------------------------\n"
-    
+    order_string += (
+        "-------------------------------------------------------------------\n"
+    )
+
     try:
         order_string += "Order \n " + order_obj["Order"]
     except:
@@ -179,7 +357,12 @@ def checkLogger():
         global LAST_CHECKED, SHOULD_LOG
         if NOW - LAST_CHECKED >= 1800:
             LAST_CHECKED = NOW
-            if LoggerType.objects.filter(setting_name='SHOULD_LOG').first().setting_value == 'True':
+            if (
+                LoggerType.objects.filter(setting_name="SHOULD_LOG")
+                .first()
+                .setting_value
+                == "True"
+            ):
                 SHOULD_LOG = True
             else:
                 SHOULD_LOG = False
@@ -187,3 +370,26 @@ def checkLogger():
     except Exception as e:
         logger.error("Error checking logger.")
         return False
+
+def encrypt_string(value):
+    """
+    Standard function to encrypt a string using
+    """
+    access_key = bytes(os.environ.get("FKEY1"), "utf-8")
+    f = Fernet(access_key)
+    if value:
+        encrypted = f.encrypt(bytes(value, "utf-8"))
+    else:
+        encrypted = f.encrypt(bytes("", "utf-8"))
+    return encrypted.decode("utf-8")
+
+def decrypt_string(value):
+    """
+    Standard function to decrypt a string using
+    """
+    access_key = bytes(os.environ.get("FKEY1"), "utf-8")
+    f = Fernet(access_key)
+    decrypted = f.decrypt(bytes(value, "utf-8"))
+
+    return decrypted.decode("utf-8")
+
